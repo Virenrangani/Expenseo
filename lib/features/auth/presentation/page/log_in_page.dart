ec912096415eeda9adb3d59a02efb024a0ac19f0
viren <virenrangani7@gmail.com>
2026-04-20 18:20:28 +0530

    feat : add log in title in login page

--- lib/features/auth/presentation/page/log_in_page.dart
+++ lib/features/auth/presentation/page/log_in_page.dart
@@ -1,10 +1,20 @@
-import 'package:flutter/cupertino.dart';
+import 'package:expenseo/features/auth/presentation/widget/log_in_title.dart';
+import 'package:flutter/material.dart';
 
 class LogInPage extends StatelessWidget {
   const LogInPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              LogInTitle()
+            ],
+          )
+      )
+    );
   }
 }
