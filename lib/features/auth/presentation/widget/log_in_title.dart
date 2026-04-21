20bce28de00d1b510f462e9019ed18e5f7fda244
viren <virenrangani7@gmail.com>
2026-04-21 10:31:10 +0530

    feat : add the app title with text style

--- lib/features/auth/presentation/widget/log_in_title.dart
+++ lib/features/auth/presentation/widget/log_in_title.dart
@@ -1,6 +1,7 @@
 import 'package:expenseo/core/constant/string/app_string.dart';
 import 'package:expenseo/core/constant/text_style/app_text_style.dart';
 import 'package:flutter/cupertino.dart';
+import '../../../../core/widget/app_title/app_title.dart';
 
 class LogInTitle extends StatelessWidget {
   const LogInTitle({super.key});
@@ -10,8 +11,8 @@ class LogInTitle extends StatelessWidget {
     return Center(
       child: Column(
         children: [
-          Text(AppString.appName,style: AppTextStyles.h1(),),
-          Text(AppString.appIntro,style: AppTextStyles.captionMedium(),)
+          AppTitle(style: AppTextStyles.h1(),),
+          Text(AppString.appIntro,style: AppTextStyles.captionMedium())
         ],
       ),
     );
