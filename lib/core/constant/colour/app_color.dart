29664d3411e8f06828758e90aca148eddc067485
viren <virenrangani7@gmail.com>
2026-04-21 11:22:50 +0530

    refactor : change the colors

--- lib/core/constant/colour/app_color.dart
+++ lib/core/constant/colour/app_color.dart
@@ -11,6 +11,7 @@ class AppColor {
   static const Color tertiary=Color(0xFF5D3FD3);
   // Background Colors
   static const Color background = Color(0xFFFFFFFF);
+  static const Color backgroundLight = Color(0xFFEDEAEA);
 
   static const Color textPrimary = Color(0xFF1A1C1E);
   static const Color textSecondary = Color(0xFF757575);
