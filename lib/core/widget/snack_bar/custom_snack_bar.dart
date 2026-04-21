61f5ef0483623b0683c7f6a8e0d8eec2acb2900f
viren <virenrangani7@gmail.com>
2026-04-21 16:21:08 +0530

    feat : add the success , error and information snack bar

---
+++ lib/core/widget/snack_bar/custom_snack_bar.dart
@@ -0,0 +1,69 @@
+import 'package:expenseo/core/constant/border_radius/app_border_radius.dart';
+import 'package:expenseo/core/constant/text_style/app_text_style.dart';
+import 'package:flutter/material.dart';
+import '../../constant/colour/app_color.dart';
+
+class CustomSnacksBar {
+
+  static void showSuccess(BuildContext context, String message) {
+    showSnackBar(
+      context,
+      message,
+      backgroundColor: AppColor.success,
+      icon: Icons.check_circle,
+    );
+  }
+
+  static void showError(BuildContext context, String message) {
+    showSnackBar(
+      context,
+      message,
+      backgroundColor: AppColor.error,
+      icon: Icons.error,
+    );
+  }
+
+  static void showInfo(BuildContext context, String message) {
+    showSnackBar(
+      context,
+      message,
+      backgroundColor: AppColor.info,
+      icon: Icons.info,
+    );
+  }
+
+  static void showSnackBar(
+      BuildContext context,
+      String message,
+      {
+        required Color backgroundColor,
+        required IconData icon,
+      }) {
+    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        behavior: SnackBarBehavior.floating,
+        backgroundColor: backgroundColor,
+        elevation: 6,
+        margin: const EdgeInsets.all(16),
+        shape: RoundedRectangleBorder(
+          borderRadius: AppBorderRadius.cir16,
+        ),
+        duration: const Duration(seconds: 3),
+        content: Row(
+          children: [
+            Icon(icon, color: AppColor.secondary),
+            const SizedBox(width: 12),
+            Expanded(
+              child: Text(
+                message,
+                style: AppTextStyles.bodySmall()
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
