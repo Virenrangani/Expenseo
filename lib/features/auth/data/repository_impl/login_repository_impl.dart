1b950020d8684d034f06dfdbe4be68119102e970
viren <virenrangani7@gmail.com>
2026-04-21 15:56:23 +0530

    feat : implementing the login method

---
+++ lib/features/auth/data/repository_impl/login_repository_impl.dart
@@ -0,0 +1,16 @@
+import 'package:expenseo/features/auth/data/data_source/login_data_source.dart';
+import 'package:expenseo/features/auth/domain/entity/user.dart';
+import 'package:expenseo/features/auth/domain/repository/log_in_repository.dart';
+
+class LoginRepositoryImpl extends LogInRepository{
+  final LoginDataSource dataSource;
+  LoginRepositoryImpl(this.dataSource);
+  @override
+  Future<User> login(String email, String password) async{
+    final result=await dataSource.login(email, password);
+    return User(
+        id:result.id ,
+        email: result.email
+    );
+  }
+}
