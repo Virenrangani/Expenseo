34fe407048fa2caf09803f67e0330b9a6c12011c
viren <virenrangani7@gmail.com>
2026-04-21 17:13:52 +0530

    feat : add password and email validation logic in the cubit

--- lib/features/auth/presentation/cubit/login_cubit.dart
+++ lib/features/auth/presentation/cubit/login_cubit.dart
@@ -1,11 +1,39 @@
 import 'package:expenseo/features/auth/presentation/cubit/auth_state.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../../core/validation/email_validation/email_password_validation.dart';
+import '../../../../core/validation/password_validation/password_validation.dart';
 import '../../domain/use_case/login_use_case.dart';
 
 class LoginCubit extends Cubit<AuthState>{
   final LoginUseCase loginUseCase;
   LoginCubit(this.loginUseCase) : super(AuthInitial());
+
+  bool emailTouched = false;
+  bool passwordTouched = false;
+
+  String? emailError;
+  String? passwordError;
+
+
+  void emailValidation(String value) {
+    emailTouched=true;
+    emailError = validateEmail(value);
+    emit(AuthFormValid());
+  }
+
+  void passwordValidation(String value) {
+    passwordTouched=true;
+    passwordError = validatePassword(value);
+    emit(AuthFormValid());
+  }
+
+  bool get isFormValid {
+    return emailError == null
+        && passwordError == null
+        && emailTouched
+        && passwordTouched;
+  }
 
   Future<void> login({
     required String email,
