49c7f4fa06fb58b19793fe70925d1466d0fe553b
viren <virenrangani7@gmail.com>
2026-04-22 16:18:11 +0530

    fix : remove the provided style in app title

--- lib/features/auth/presentation/widget/log_in_title.dart
+++ lib/features/auth/presentation/widget/log_in_title.dart
@@ -1,6 +1,7 @@
 import 'package:expenseo/core/constant/string/app_string.dart';
 import 'package:expenseo/core/constant/text_style/app_text_style.dart';
 import 'package:flutter/cupertino.dart';
+
 import '../../../../core/widget/app_title/app_title.dart';
 
 class LogInTitle extends StatelessWidget {
@@ -11,8 +12,8 @@ class LogInTitle extends StatelessWidget {
     return Center(
       child: Column(
         children: [
-          AppTitle(style: AppTextStyles.h1(),),
-          Text(AppString.appIntro,style: AppTextStyles.captionMedium())
+          AppTitle(),
+          Text(AppString.appIntro, style: AppTextStyles.captionMedium()),
         ],
       ),
     );
