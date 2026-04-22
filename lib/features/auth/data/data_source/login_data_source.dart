72ed024a07226ef21cfd88bf3b2a05c176df82fe
viren <virenrangani7@gmail.com>
2026-04-22 17:05:53 +0530

    feat :add the firebase exception

--- lib/features/auth/data/data_source/login_data_source.dart
+++ lib/features/auth/data/data_source/login_data_source.dart
@@ -1,14 +1,16 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+
 import '../../../../core/constant/string/app_string.dart';
+import '../../../../core/error/app_errors.dart';
 import '../model/user_model.dart';
 
 abstract class LoginDataSource {
-  Future<UserModel> login(String email,String password);
+  Future<UserModel> login(String email, String password);
   Future<UserModel> signInWithGoogle();
 }
 
-class LoginDataSourceImpl extends LoginDataSource{
+class LoginDataSourceImpl extends LoginDataSource {
   final FirebaseAuth firebaseAuth;
 
   LoginDataSourceImpl(this.firebaseAuth);
@@ -28,19 +30,19 @@ class LoginDataSourceImpl extends LoginDataSource{
       }
 
       return UserModel(
-          id: user.uid,
-          email: user.email ??"",
-          name: user.displayName ??""
+        id: user.uid,
+        email: user.email ?? "",
+        name: user.displayName ?? "",
       );
-    } on FirebaseAuthException {
-      rethrow;
+    } on FirebaseAuthException catch (e) {
+      throw Exception(AppErrors.handleException(e));
     } catch (e) {
-      rethrow;
+      throw Exception(AppString.somethingWentWrong);
     }
   }
 
   @override
-  Future<UserModel> signInWithGoogle() async{
+  Future<UserModel> signInWithGoogle() async {
     try {
       await GoogleSignIn().signOut();
       final googleUser = await GoogleSignIn().signIn();
@@ -53,21 +55,22 @@ class LoginDataSourceImpl extends LoginDataSource{
         idToken: googleAuth.idToken,
       );
 
-      final userCredential = await firebaseAuth.signInWithCredential(credential);
+      final userCredential = await firebaseAuth.signInWithCredential(
+        credential,
+      );
       final user = userCredential.user;
 
       if (user == null) throw Exception(AppString.userNotFound);
 
       return UserModel(
-          id: user.uid,
-          email: user.email ??"",
-          name: user.displayName ??""
+        id: user.uid,
+        email: user.email ?? "",
+        name: user.displayName ?? "",
       );
-    } on FirebaseAuthException {
-      rethrow;
+    } on FirebaseAuthException catch (e) {
+      throw Exception(AppErrors.handleException(e));
     } catch (e) {
-      rethrow;
+      throw Exception(AppString.somethingWentWrong);
     }
   }
-
 }
