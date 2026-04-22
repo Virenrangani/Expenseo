fdbb71f2fe0b28ca2b4053c1e345b73eba323c0e
viren <virenrangani7@gmail.com>
2026-04-22 17:50:52 +0530

    feat : add have a nice day and hello string

--- lib/core/constant/string/app_string.dart
+++ lib/core/constant/string/app_string.dart
@@ -1,48 +1,52 @@
 class AppString {
+  static const appName = "Expenseo";
+  static const appIntro = "Enter your fluid vault.";
 
-  static const appName="Expenseo";
-  static const appIntro="Enter your fluid vault.";
+  static const logInIntro = "Welcome back";
+  static const logInSubIntro = "Sign in to continue your narrative.";
 
-  static const logInIntro="Welcome back";
-  static const logInSubIntro="Sign in to continue your narrative.";
+  static const email = "Email address";
 
-  static const email="Email address";
+  static const String password = "Password";
+  static const String forgotPassword = "Forgot Password?";
 
-  static const String password="Password";
-  static const String forgotPassword="Forgot Password?";
+  static const String signIN = "Sign In";
+  static const String signInWithGoogle = "Sign In with Google";
 
-  static const String signIN="Sign In";
-  static const String signInWithGoogle="Sign In with Google";
+  static const String dontHaveAnAccount = "Don't have an account? ";
+  static const String signUp = "Sign up";
 
-  static const String dontHaveAnAccount="Don't have an account? ";
-  static const String signUp="Sign up";
+  static const String alReadyHaveAnAccount = "Already have an account? ";
+  static const String somethingWentWrong = "Something went to wrong";
 
-  static const String alReadyHaveAnAccount="Already have an account? ";
-  static const String somethingWentWrong="Something went to wrong";
+  static const signUpIntro = "Create Your Narrative";
+  static const signUpSubIntro = "Start your journey to financial clarity.";
 
-  static const signUpIntro="Create Your Narrative";
-  static const signUpSubIntro="Start your journey to financial clarity.";
+  static const name = "Name";
+  static const createAccount = "Create Account";
 
-  static const name="Name";
-  static const createAccount="Create Account";
+  static const String userNotVerify = "user is not verified";
+  static const String userLogin = "User is Login Successfully";
 
-  static const String userNotVerify="user is not verified";
-  static const String userLogin="User is Login Successfully";
+  static const String emailRequired = "Email is required";
+  static const String emailHint = "Enter Mail";
+  static const String emailNotContainsSpace = "Email should not contain spaces";
+  static const String emailValid = "Enter a valid email";
 
-  static const String emailRequired="Email is required";
-  static const String emailHint="Enter Mail";
-  static const String emailNotContainsSpace="Email should not contain spaces";
-  static const String emailValid="Enter a valid email";
+  static const String passwordRequired = "Password is required";
+  static const String passwordHint = "Enter Password";
+  static const String passwordNotContainsSpace =
+      "Password should not contain spaces";
+  static const String passwordMinChar = "Minimum 8 characters required";
+  static const String atLeastOneUpperCase = "Add at least one uppercase letter";
+  static const String atLeastOneLowerCase = "Add at least one lowercase letter";
+  static const String atLeastOneNumber = "Add at least one number";
+  static const String atLeastOneSpecialChar =
+      "Add at least one special character";
 
-  static const String passwordRequired="Password is required";
-  static const String passwordHint="Enter Password";
-  static const String passwordNotContainsSpace="Password should not contain spaces";
-  static const String passwordMinChar="Minimum 8 characters required";
-  static const String atLeastOneUpperCase="Add at least one uppercase letter";
-  static const String atLeastOneLowerCase="Add at least one lowercase letter";
-  static const String atLeastOneNumber="Add at least one number";
-  static const String atLeastOneSpecialChar="Add at least one special character";
+  static const String googleSignInFailed = "Google sign in failed";
+  static const String userNotFound = "User is not found";
 
-  static const String googleSignInFailed="Google sign in failed";
-  static const String userNotFound="User is not found";
+  static const String homeIntro = "Have a nice day";
+  static const String hello = "Hello";
 }
