d1955b2682603a6a6839d663b13e5f88b1dd583d
viren <virenrangani7@gmail.com>
2026-04-22 11:08:39 +0530

    feat : add all firebase logic in data source

---
+++ lib/features/auth/data/data_source/sign_up_data_source.dart
@@ -0,0 +1,37 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import '../../../../core/constant/string/app_string.dart';
+import '../model/user_model.dart';
+
+abstract class SignUpDataSource {
+  Future<UserModel> signUp(String email, String name, String password);
+}
+
+class SignUpDataSourceImpl implements SignUpDataSource{
+  final FirebaseAuth firebaseAuth;
+  SignUpDataSourceImpl(this.firebaseAuth);
+
+  Future<UserModel> signUp(String email, String name, String password) async {
+    try {
+      UserCredential credential = await firebaseAuth
+          .createUserWithEmailAndPassword(email: email, password: password);
+
+      final user = credential.user;
+      if (user == null) throw Exception(AppString.userNotFound);
+
+      await user.updateDisplayName(name);
+      await user.reload();
+
+      await user.sendEmailVerification();
+
+      return UserModel(
+          id: user.uid,
+          email: user.email??"",
+          name: user.displayName??""
+      );
+    } on FirebaseAuthException {
+      rethrow;
+    } catch (e) {
+      throw Exception(AppString.somethingWentWrong);
+    }
+  }
+}
