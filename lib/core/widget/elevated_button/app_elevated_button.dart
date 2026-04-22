da993323985121b87225c95346806df486eb64d5
viren <virenrangani7@gmail.com>
2026-04-22 16:26:22 +0530

    refactor : pass the on pressed function directly remove the handle tap function

--- lib/core/widget/elevated_button/app_elevated_button.dart
+++ lib/core/widget/elevated_button/app_elevated_button.dart
@@ -1,13 +1,12 @@
-import 'dart:ffi';
-
 import 'package:expenseo/core/constant/gap/app_gap.dart';
 import 'package:flutter/material.dart';
+
 import '../../constant/colour/app_color.dart';
 import '../../constant/text_style/app_text_style.dart';
 
-class CustomElevatedButton extends StatelessWidget {
+class AppElevatedButton extends StatelessWidget {
   final String text;
-  final VoidCallback onPressed;
+  final VoidCallback? onPressed;
   final bool isLoading;
   final double? width;
   final double? height;
@@ -18,26 +17,20 @@ class CustomElevatedButton extends StatelessWidget {
   final IconData? prefixIcon;
   final IconData? suffixIcon;
 
-  const CustomElevatedButton({
+  const AppElevatedButton({
     super.key,
     required this.text,
     required this.onPressed,
     this.isLoading = false,
     this.width,
-    this.height=52,
-    this.borderRadius=24,
+    this.height = 52,
+    this.borderRadius = 24,
     this.color1,
     this.color2,
-    this.isEnabled=false,
+    this.isEnabled = false,
     this.prefixIcon,
     this.suffixIcon,
   });
-
-  void handleTap() async {
-    if (isLoading || !isEnabled) return;
-
-    onPressed();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,37 +38,38 @@ class CustomElevatedButton extends StatelessWidget {
       width: width ?? MediaQuery.of(context).size.width,
       height: height,
       child: ElevatedButton(
-        onPressed: (!isEnabled || isLoading) ? null : handleTap,
+        onPressed: (!isEnabled || isLoading) ? null : onPressed,
         style: ElevatedButton.styleFrom(
           backgroundColor: color1 ?? AppColor.primary,
-          disabledBackgroundColor: color1 ??AppColor.primary,
+          disabledBackgroundColor: color1 ?? AppColor.primary,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(borderRadius!),
           ),
         ),
         child: isLoading
             ? SizedBox(
-          height: 22,
-          width: 22,
-          child: CircularProgressIndicator(
-            strokeWidth: 2,
-            color: AppColor.divider,
-          ),
-        )
-            : Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                if(prefixIcon != null)
-                  Icon(prefixIcon,color: AppColor.background,size:22,),
-                AppGap.g4,
-                Text(
-                text, style: AppTextStyles.h4(color:AppColor.background)
+                height: 22,
+                width: 22,
+                child: CircularProgressIndicator(
+                  strokeWidth: 2,
+                  color: AppColor.divider,
                 ),
-                AppGap.g4,
-                if(suffixIcon != null)
-                  Icon(suffixIcon,color: AppColor.background,size:22,),
-              ],
-            ),
+              )
+            : Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  if (prefixIcon != null)
+                    Icon(prefixIcon, color: AppColor.background, size: 22),
+                  AppGap.g4,
+                  Text(
+                    text,
+                    style: AppTextStyles.h4(color: AppColor.background),
+                  ),
+                  AppGap.g4,
+                  if (suffixIcon != null)
+                    Icon(suffixIcon, color: AppColor.background, size: 22),
+                ],
+              ),
       ),
     );
   }
