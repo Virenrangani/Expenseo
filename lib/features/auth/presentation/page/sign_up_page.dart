220f94999834e93a072600c135f70d6d246449f7
viren <virenrangani7@gmail.com>
2026-04-22 11:26:51 +0530

    feat : add provider to provide sign up cubit and also use the bloc consumer

--- lib/features/auth/presentation/page/sign_up_page.dart
+++ lib/features/auth/presentation/page/sign_up_page.dart
@@ -9,6 +9,11 @@ import '../../../../core/constant/string/app_string.dart';
 import '../../../../core/constant/text_style/app_text_style.dart';
 import '../../../../core/widget/elevated_button/app_elevated_button.dart';
 import '../../../../core/widget/text_field/app_text_field.dart';
+import 'package:expenseo/features/auth/presentation/cubit/auth_state.dart';
+import 'package:expenseo/features/auth/presentation/cubit/sign_up_cubit.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
+import 'package:expenseo/core/widget/snack_bar/custom_snack_bar.dart';
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
@@ -35,109 +40,125 @@ class _SignUpPageState extends State<SignUpPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColor.primaryLight,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          physics: BouncingScrollPhysics(),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              AppTitle(style: AppTextStyles.h1()),
-              AppGap.g20,
+    return BlocProvider(
+      create: (context) => GetIt.I<SignUpCubit>(),
+      child: Scaffold(
+        backgroundColor: AppColor.primaryLight,
+        body: BlocConsumer<SignUpCubit, AuthState>(
+          listener: (context, state) {
+              if(state is AuthFailure){
+                return CustomSnacksBar.showError(context, state.message);
+              }
+              if(state is AuthSuccess){
+                return CustomSnacksBar.showSuccess(context, AppString.userLogin);
+              }
+          },
+          builder: (context, state) {
+            return SafeArea(
+              child: SingleChildScrollView(
+                physics: BouncingScrollPhysics(),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    AppTitle(style: AppTextStyles.h1()),
+                    AppGap.g20,
 
-              Padding(
-                padding: AppPadding.edgeAll16,
-                child: Container(
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: AppColor.background,
-                    borderRadius: AppBorderRadius.cir12,
-                  ),
-                  child: Padding(
-                    padding: AppPadding.edgeSymmetricHori24,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        AppGap.g32,
-                        Text(
-                          AppString.signUpIntro,
-                          style: AppTextStyles.h3(color: AppColor.textPrimary),
+                    Padding(
+                      padding: AppPadding.edgeAll16,
+                      child: Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: AppColor.background,
+                          borderRadius: AppBorderRadius.cir12,
                         ),
-                        AppGap.g4,
-                        Text(
-                          AppString.signUpSubIntro,
-                          style: AppTextStyles.bodySmall(),
-                        ),
-                        AppGap.g32,
-                        AppFormField(
-                          controller: nameController,
-                          hintText: AppString.name,
-                          labelText: AppString.name,
-                        ),
-                        AppGap.g24,
-                        AppFormField(
-                          controller: emailController,
-                          hintText: AppString.email,
-                          labelText: AppString.email,
-                        ),
-                        AppGap.g24,
-                        AppFormField(
-                          controller: passwordController,
-                          hintText: AppString.password,
-                          labelText: AppString.password,
-                          obscureText: isPasswordHidden,
-                          suffix: Icon(
-                            isPasswordHidden
-                                ? Icons.visibility_off
-                                : Icons.visibility,
+                        child: Padding(
+                          padding: AppPadding.edgeSymmetricHori24,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              AppGap.g32,
+                              Text(
+                                AppString.signUpIntro,
+                                style: AppTextStyles.h3(
+                                    color: AppColor.textPrimary),
+                              ),
+                              AppGap.g4,
+                              Text(
+                                AppString.signUpSubIntro,
+                                style: AppTextStyles.bodySmall(),
+                              ),
+                              AppGap.g32,
+                              AppFormField(
+                                controller: nameController,
+                                hintText: AppString.name,
+                                labelText: AppString.name,
+                              ),
+                              AppGap.g24,
+                              AppFormField(
+                                controller: emailController,
+                                hintText: AppString.email,
+                                labelText: AppString.email,
+                              ),
+                              AppGap.g24,
+                              AppFormField(
+                                controller: passwordController,
+                                hintText: AppString.password,
+                                labelText: AppString.password,
+                                obscureText: isPasswordHidden,
+                                suffix: Icon(
+                                  isPasswordHidden
+                                      ? Icons.visibility_off
+                                      : Icons.visibility,
+                                ),
+                                onSuffixTap: () {
+                                  setState(() {
+                                    isPasswordHidden = !isPasswordHidden;
+                                  });
+                                },
+                              ),
+                              AppGap.g32,
+                              CustomElevatedButton(
+                                text: AppString.createAccount,
+                                suffixIcon: Icons.arrow_forward_outlined,
+                                onPressed: () {},
+                              ),
+                              AppGap.g32,
+                            ],
                           ),
-                          onSuffixTap: () {
-                            setState(() {
-                              isPasswordHidden = !isPasswordHidden;
-                            });
-                          },
                         ),
-                        AppGap.g32,
-                        CustomElevatedButton(
-                          text: AppString.createAccount,
-                          suffixIcon: Icons.arrow_forward_outlined,
-                          onPressed: () {},
-                        ),
-                        AppGap.g32,
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              AppGap.g32,
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    AppString.alReadyHaveAnAccount,
-                    style: AppTextStyles.bodyMedium(),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const LogInPage(),
-                        ),
-                      );
-                    },
-                    child: Text(
-                      AppString.signIN,
-                      style: AppTextStyles.bodyMedium(
-                        color: AppColor.secondary,
                       ),
                     ),
-                  ),
-                ],
+                    AppGap.g32,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          AppString.alReadyHaveAnAccount,
+                          style: AppTextStyles.bodyMedium(),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const LogInPage(),
+                              ),
+                            );
+                          },
+                          child: Text(
+                            AppString.signIN,
+                            style: AppTextStyles.bodyMedium(
+                              color: AppColor.secondary,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ],
-          ),
+            );
+          },
         ),
       ),
     );
