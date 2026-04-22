976f95777a348db7d60c92d881298123e8496a44
viren <virenrangani7@gmail.com>
2026-04-22 16:17:19 +0530

    feat : add default text style

--- lib/core/widget/app_title/app_title.dart
+++ lib/core/widget/app_title/app_title.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/cupertino.dart';
+
 import '../../constant/string/app_string.dart';
+import '../../constant/text_style/app_text_style.dart';
 
 class AppTitle extends StatelessWidget {
+  final TextStyle? style;
 
-  final TextStyle style;
-
-   AppTitle({
-    super.key,
-    required this.style
-  });
+  const AppTitle({super.key, this.style});
 
   @override
   Widget build(BuildContext context) {
-    return Text(AppString.appName,style: style,);
+    return Text(AppString.appName, style: style ?? AppTextStyles.h1());
   }
 }
