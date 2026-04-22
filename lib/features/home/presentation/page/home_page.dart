221f8f4ebec44ae746f9abb72c95d8386ce068ab
viren <virenrangani7@gmail.com>
2026-04-22 15:04:33 +0530

    feat : add app title in home page

--- lib/features/home/presentation/page/home_page.dart
+++ lib/features/home/presentation/page/home_page.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import '../../../../core/constant/text_style/app_text_style.dart';
+import '../../../../core/widget/app_title/app_title.dart';
+import '../../../../core/constant/colour/app_color.dart';
+import '../../../../core/constant/padding/app_padding.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+          child:Padding(
+              padding:AppPadding.edgeSymmetricHori16,
+          child:Column(
+            children: [
+              AppTitle(style: AppTextStyles.h3(color:AppColor.primary)),
+            ],
+          )
+          ),
+      )
+    );
   }
 }
