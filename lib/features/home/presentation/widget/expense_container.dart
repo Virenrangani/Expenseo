aee51b387b3dcf30fc24d46b6009e3b6c52502a0
viren <virenrangani7@gmail.com>
2026-04-23 15:10:33 +0530

    feat : add tab selector user can select the balance , income , expense

--- lib/features/home/presentation/widget/expense_container.dart
+++ lib/features/home/presentation/widget/expense_container.dart
@@ -1,32 +1,39 @@
 import 'package:flutter/material.dart';
-import '../../../../core/constant/border_radius/app_border_radius.dart';
 import '../../../../core/constant/colour/app_color.dart';
-import '../../../../core/constant/gap/app_gap.dart';
 import '../../../../core/constant/padding/app_padding.dart';
-import '../../../../core/constant/string/app_string.dart';
-import '../../../../core/constant/text_style/app_text_style.dart';
+import 'package:expenseo/features/home/presentation/widget/tab_selector.dart';
+import '../../../../core/constant/gap/app_gap.dart';
+import 'package:expenseo/features/home/presentation/widget/show_amount.dart';
+import 'package:expenseo/features/home/presentation/widget/expense_stats.dart';
 
-class ExpenseContainer extends StatelessWidget {
+enum ExpenseTab { balance, income, expense }
+
+class ExpenseContainer extends StatefulWidget {
   const ExpenseContainer({super.key});
+
+  @override
+  State<ExpenseContainer> createState() => _ExpenseContainerState();
+}
+
+class _ExpenseContainerState extends State<ExpenseContainer> {
+  ExpenseTab _selectedTab = ExpenseTab.balance;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       decoration: BoxDecoration(
-          borderRadius: AppBorderRadius.cir16,
-          color: AppColor.primary
+        borderRadius: BorderRadius.circular(20),
+        color: AppColor.primary,
       ),
+      padding: AppPadding.edgeAll12,
       child: Column(
         children: [
-          AppGap.g16,
-          Text("My Expense", style: AppTextStyles.bodyLarge()),
-          AppGap.g8,
-          Text(
-            "\$ 23455",
-            style: AppTextStyles.h3(color: AppColor.background),
+          TabSelector(
+            selectedTab: _selectedTab,
+            onTabChanged: (tab) => setState(() => _selectedTab = tab),
           ),
-          AppGap.g16,
+          AppGap.g24,
         ],
       ),
     );
