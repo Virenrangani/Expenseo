fd5c5b274d411653d51f7ec08cffed384b48547b
viren <virenrangani7@gmail.com>
2026-04-23 17:43:27 +0530

    fix : fix in signup method to return the user

--- lib/features/auth/domain/use_case/sign_up_use_case.dart
+++ lib/features/auth/domain/use_case/sign_up_use_case.dart
@@ -1,10 +1,11 @@
 import 'package:expenseo/features/auth/domain/repository/sign_up_repository.dart';
+import 'package:expenseo/features/auth/domain/entity/user.dart';
 
 class SignUpUseCase {
   final SignUpRepository signUpRepository;
   SignUpUseCase(this.signUpRepository);
 
-  Future<void> signUpWithEmail(String name, String email, String password) {
+  Future<User> signUpWithEmail(String name, String email, String password) {
     return signUpRepository.signUp(name, email, password);
   }
 }
