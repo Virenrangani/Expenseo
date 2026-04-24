bed1bdaf4129dc6ce87c347bdf99ff4d8a45399d
viren <virenrangani7@gmail.com>
2026-04-24 09:53:54 +0530

    feat : make class stateful and fetch username using the shared pref get username method

--- lib/features/home/presentation/page/home_page.dart
+++ lib/features/home/presentation/page/home_page.dart
@@ -5,15 +5,36 @@ import '../../../../core/constant/padding/app_padding.dart';
 import '../../../../core/constant/string/app_string.dart';
 import '../../../../core/constant/text_style/app_text_style.dart';
 import '../../../../core/widget/app_title/app_title.dart';
-import 'package:expenseo/features/home/presentation/widget/expense_container.dart';
 import '../../../../core/widget/app_icon_card/app_icon_card.dart';
 import 'package:expenseo/features/home/presentation/widget/transaction_list.dart';
 import 'package:expenseo/features/home/presentation/widget/greeting_user.dart';
+import 'package:expenseo/features/home/presentation/widget/expense_container.dart';
 import 'package:expenseo/features/expense/presentation/page/user_expense_page.dart';
 import 'package:expenseo/features/split_expense/presentation/page/split_expense_page.dart';
+import '../../../../core/storage/shared_pref/shared_pref_service.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String userName = "";
+
+  @override
+  void initState() {
+    super.initState();
+    loadUserName();
+  }
+
+  Future<void> loadUserName() async {
+    final name = await SharedPrefService.getUserName();
+    setState(() {
+      userName = name ?? "User";
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +47,7 @@ class HomePage extends StatelessWidget {
             children: [
               AppTitle(style: AppTextStyles.h3(color: AppColor.secondary)),
               AppGap.g16,
-              GreetingUser(userName: "Viren"),
+              GreetingUser(userName: userName),
               AppGap.g20,
               ExpenseContainer(),
               AppGap.g24,
