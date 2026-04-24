e20d91529e59b011456c45fb37e8b42c7f18d99d
viren <virenrangani7@gmail.com>
2026-04-24 17:36:24 +0530

    feat : add the Expense page dependency

--- lib/di/injection.dart
+++ lib/di/injection.dart
@@ -10,6 +10,13 @@ import 'package:expenseo/features/auth/data/repository_impl/signup_repository_im
 import 'package:expenseo/features/auth/domain/repository/sign_up_repository.dart';
 import 'package:expenseo/features/auth/domain/use_case/sign_up_use_case.dart';
 import '../features/auth/presentation/cubit/sign_up_cubit.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:expenseo/features/expense/data/data_source/expense_data_source.dart';
+import 'package:expenseo/features/expense/data/repository_impl/expense_repository_impl.dart';
+import 'package:expenseo/features/expense/domain/repository/expense_repository.dart';
+import 'package:expenseo/features/expense/domain/use_case/expense_use_case.dart';
+import 'package:expenseo/features/expense/presentation/cubit/expense_cubit.dart';
+
 
 class Injection {
   final GetIt sl=GetIt.instance;
@@ -17,6 +24,7 @@ class Injection {
   void configDependency(){
 
     sl.registerLazySingleton(()=>FirebaseAuth.instance);
+    sl.registerLazySingleton(()=>FirebaseFirestore.instance);
 
     sl.registerLazySingleton<LoginDataSource>(()=>LoginDataSourceImpl(sl()));
     sl.registerLazySingleton<LogInRepository>(()=>LoginRepositoryImpl(sl()));
@@ -27,5 +35,10 @@ class Injection {
     sl.registerLazySingleton<SignUpRepository>(()=>SignUpRepositoryImpl(sl()));
     sl.registerLazySingleton(()=>SignUpUseCase(sl()));
     sl.registerFactory(()=>SignUpCubit(sl()));
+
+    sl.registerLazySingleton<ExpenseDataSource>(()=>ExpenseDataSourceImpl(sl()));
+    sl.registerLazySingleton<ExpenseRepository>(()=>ExpenseRepositoryImpl(sl()));
+    sl.registerLazySingleton(()=>ExpenseUseCase(sl()));
+    sl.registerFactory(()=>ExpenseCubit(sl()));
   }
 }
