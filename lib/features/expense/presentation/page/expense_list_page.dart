189ba36d1369bab4097700e836cdf679ff2750d8
viren <virenrangani7@gmail.com>
2026-04-27 14:47:15 +0530

    feat : add groupByDate map function that group the expense using the dataLabel

--- lib/features/expense/presentation/page/expense_list_page.dart
+++ lib/features/expense/presentation/page/expense_list_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import '../../../../core/widget/date_label/date_label.dart';
 import '../../domain/entity/expense.dart';
+import '../widget/expense_empty_list_screen.dart';
 
 class ExpenseListPage extends StatelessWidget {
   final List<Expense> expenses;
@@ -11,6 +13,19 @@ class ExpenseListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    if (expenses.isEmpty) return ExpenseEmptyListScreen();
+
+    final grouped = _groupByDate(expenses);
+
+    return Container();
+
+  }
+  Map<String, List<Expense>> _groupByDate(List<Expense> list) {
+    final Map<String, List<Expense>> map = {};
+    for (final e in list) {
+      final key = dateLabel(e.createdAt);
+      map.putIfAbsent(key, () => []).add(e);
+    }
+    return map;
   }
 }
