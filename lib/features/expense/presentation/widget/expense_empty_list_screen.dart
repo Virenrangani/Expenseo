9a1b26bf8f16a7a47266952a51e8d7d0dc84888d
viren <virenrangani7@gmail.com>
2026-04-27 14:42:19 +0530

    feat : add expense empty screen when no expense then show

---
+++ lib/features/expense/presentation/widget/expense_empty_list_screen.dart
@@ -0,0 +1,28 @@
+import 'package:expenseo/core/constant/string/app_string.dart';
+import 'package:flutter/material.dart';
+import '../../../../core/constant/colour/app_color.dart';
+import '../../../../core/constant/gap/app_gap.dart';
+import '../../../../core/constant/text_style/app_text_style.dart';
+
+class ExpenseEmptyListScreen extends StatelessWidget {
+  const ExpenseEmptyListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(Icons.receipt_long_outlined,
+              size: 64, color: AppColor.divider),
+          AppGap.g16,
+          Text(AppString.noExpenseYet,
+              style: AppTextStyles.h5(color: AppColor.textSecondary)),
+          AppGap.g8,
+          Text(AppString.addFirstExpense,
+              style: AppTextStyles.caption(color: AppColor.textSecondary)),
+        ],
+      ),
+    );
+  }
+}
