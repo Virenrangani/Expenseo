3fa9d19e4554da130e7d682be522f4c38bdfe1ae
viren <virenrangani7@gmail.com>
2026-04-27 14:54:37 +0530

    feat : add option variable to find which category's data load and isExpense to track type of transaction

--- lib/features/expense/presentation/widget/expense_card.dart
+++ lib/features/expense/presentation/widget/expense_card.dart
@@ -1,12 +1,5 @@
 import 'package:flutter/cupertino.dart';
-import '../../../../core/constant/border_radius/app_border_radius.dart';
-import '../../../../core/constant/colour/app_color.dart';
-import '../../../../core/constant/gap/app_gap.dart';
-import '../../../../core/constant/padding/app_padding.dart';
-import '../../../../core/constant/text_style/app_text_style.dart';
 import '../../../../core/enums/app_enums.dart';
-import '../../../../core/widget/format_amount/format_amount.dart';
-import '../../../../core/widget/format_date/format_date.dart';
 import '../../domain/entity/expense.dart';
 import 'category_list_option.dart';
 
@@ -17,7 +10,9 @@ class ExpenseCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
+    final option = categoryListOption[expense.category]!;
+    final isExpense = expense.type == TransactionType.expense;
+
     return Container();
   }
 }
