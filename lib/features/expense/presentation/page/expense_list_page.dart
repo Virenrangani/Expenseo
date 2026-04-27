1559fa32c9142d631cc62e1067e7307a99610110
viren <virenrangani7@gmail.com>
2026-04-27 15:06:47 +0530

    feat : using the groupByDate and list all expense use the ExpanseCard to show the Expense data

--- lib/features/expense/presentation/page/expense_list_page.dart
+++ lib/features/expense/presentation/page/expense_list_page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import '../../../../core/constant/colour/app_color.dart';
+import '../../../../core/constant/gap/app_gap.dart';
+import '../../../../core/constant/padding/app_padding.dart';
+import '../../../../core/constant/text_style/app_text_style.dart';
 import '../../../../core/widget/date_label/date_label.dart';
 import '../../domain/entity/expense.dart';
+import '../widget/expense_card.dart';
 import '../widget/expense_empty_list_screen.dart';
 
 class ExpenseListPage extends StatelessWidget {
@@ -16,10 +21,31 @@ class ExpenseListPage extends StatelessWidget {
     if (expenses.isEmpty) return ExpenseEmptyListScreen();
 
     final grouped = _groupByDate(expenses);
+    return SafeArea(
+      child: ListView(
+        padding: AppPadding.edgeAll16,
+        children: grouped.entries.map((entry) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                entry.key,
+                style: AppTextStyles.captionMedium(
+                  color: AppColor.textSecondary,
+                ),
+              ),
+              AppGap.g8,
 
-    return Container();
+              ...entry.value.map((expense) => ExpenseCard(expense: expense)),
 
+              AppGap.g16,
+            ],
+          );
+        }).toList(),
+      ),
+    );
   }
+
   Map<String, List<Expense>> _groupByDate(List<Expense> list) {
     final Map<String, List<Expense>> map = {};
     for (final e in list) {
