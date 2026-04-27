95cd9ace11ab2f7430f38e416b13faf8e4731fa7
viren <virenrangani7@gmail.com>
2026-04-27 15:08:09 +0530

    feat : add bloc Builder and showing the all state according result

--- lib/features/expense/presentation/page/user_expense_page.dart
+++ lib/features/expense/presentation/page/user_expense_page.dart
@@ -1,21 +1,39 @@
+import 'package:expenseo/features/expense/presentation/cubit/expense_state.dart';
 import 'package:flutter/material.dart';
 import './add_expense_sheet.dart';
 import '../cubit/expense_cubit.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 import '../../../../core/constant/colour/app_color.dart';
+import 'expense_list_page.dart';
 
 class UserExpensePage extends StatelessWidget {
   const UserExpensePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  BlocProvider(
-      create: (_) => GetIt.I<ExpenseCubit>(),
+    return  BlocProvider<ExpenseCubit>(
+      create: (_) => GetIt.I<ExpenseCubit>()..getExpense(),
       child: Builder(
         builder: (context) {
           return Scaffold(
-            body: Container(),
+            body:BlocBuilder<ExpenseCubit,ExpenseState>(
+                builder: (context,state){
+                  if(state is ExpenseLoading){
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  if(state is ExpenseError){
+                    return Center(child: Text(state.message),);
+                  }
+                  if(state is ExpenseLoaded){
+                    return ExpenseListPage(
+                      expenses: state.expenses ?? [],
+                    );
+                  }
+                  return const SizedBox.shrink();
+                }),
 
             floatingActionButton: FloatingActionButton(
               backgroundColor: AppColor.secondary,
