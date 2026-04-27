0b4804c9edfaf871601746e5c0f604704b83e631
viren <virenrangani7@gmail.com>
2026-04-27 14:37:03 +0530

    feat : add expense list page and pass List Expense

---
+++ lib/features/expense/presentation/page/expense_list_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import '../../domain/entity/expense.dart';
+
+class ExpenseListPage extends StatelessWidget {
+  final List<Expense> expenses;
+
+  const ExpenseListPage({
+    super.key,
+    required this.expenses,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
