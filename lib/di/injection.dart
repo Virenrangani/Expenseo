020f7e2193231c22422c8971c7d3c4160a620e1c
viren <virenrangani7@gmail.com>
2026-04-28 14:07:22 +0530

    feat : add home dependency injection

--- lib/di/injection.dart
+++ lib/di/injection.dart
@@ -2,6 +2,11 @@ import 'package:expenseo/features/auth/data/data_source/login_data_source.dart';
 import 'package:expenseo/features/auth/data/repository_impl/login_repository_impl.dart';
 import 'package:expenseo/features/auth/domain/repository/log_in_repository.dart';
 import 'package:expenseo/features/auth/domain/use_case/login_use_case.dart';
+import 'package:expenseo/features/home/data/data_source/home_data_source.dart';
+import 'package:expenseo/features/home/data/repository/home_repository_impl.dart';
+import 'package:expenseo/features/home/domain/repository/home_repository.dart';
+import 'package:expenseo/features/home/domain/use_case/home_use_case.dart';
+import 'package:expenseo/features/home/presentation/cubit/home_cubit.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
 import '../features/auth/presentation/cubit/login_cubit.dart';
@@ -27,5 +32,10 @@ class Injection {
     sl.registerLazySingleton<SignUpRepository>(()=>SignUpRepositoryImpl(sl()));
     sl.registerLazySingleton(()=>SignUpUseCase(sl()));
     sl.registerFactory(()=>SignUpCubit(sl()));
+
+    sl.registerLazySingleton<HomeDataSource>(()=>HomeDataSourceImpl());
+    sl.registerLazySingleton<HomeRepository>(()=>HomeRepositoryImpl(sl()));
+    sl.registerLazySingleton(()=>HomeUseCase(sl()));
+    sl.registerFactory(()=>HomeCubit(sl()));
   }
 }
