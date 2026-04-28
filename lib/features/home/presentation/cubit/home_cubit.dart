56582dcdd69dcdc653afb3685bca441e882c84ba
viren <virenrangani7@gmail.com>
2026-04-28 14:07:05 +0530

    feat : add get username method with state and store username in variable

---
+++ lib/features/home/presentation/cubit/home_cubit.dart
@@ -0,0 +1,24 @@
+import 'package:expenseo/features/home/domain/use_case/home_use_case.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'home_state.dart';
+
+class HomeCubit extends Cubit<HomeState> {
+  final HomeUseCase homeUseCase;
+  HomeCubit(this.homeUseCase) : super(HomeInitial());
+
+  String userName="";
+
+  Future<void> getUserName()async {
+    emit(HomeLoading());
+    try{
+       final name = await homeUseCase.getUserName();
+       userName=name ?? " ";
+       emit(HomeSuccess());
+    }catch(e){
+      emit(HomeError());
+    }
+
+  }
+}
+
