24974e1c4d339c26626c0a9236940741e6f95a3d
viren <virenrangani7@gmail.com>
2026-04-28 09:39:42 +0530

    feat : add remove method with user id and expense id

--- lib/features/expense/domain/repository/expense_repository.dart
+++ lib/features/expense/domain/repository/expense_repository.dart
@@ -3,5 +3,5 @@ import '../entity/expense.dart';
 abstract class ExpenseRepository {
   Future<void> addExpense(String uid, Expense expense);
   Future<List<Expense>> getExpense(String uid);
-  // Future<void> removeExpense();
+  Future<void> removeExpense(String uid,String expenseId);
 }
