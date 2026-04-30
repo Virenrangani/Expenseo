e81a43997c3a519a3d6e9388dd7589a5c11af8db
viren <virenrangani7@gmail.com>
2026-04-30 14:52:40 +0530

    feat: add index to track months

--- lib/features/calendar/presentation/cubit/calendar_state.dart
+++ lib/features/calendar/presentation/cubit/calendar_state.dart
@@ -8,16 +8,19 @@ class CalendarLoading extends CalendarState {}
 class CalendarLoaded extends CalendarState {
   final int year;
   final int month;
+  final int index;
 
-  CalendarLoaded({ required this.year,  required this.month});
+  CalendarLoaded({ required this.year,  required this.month, required this.index});
 
   CalendarLoaded copyWith({
     int? year,
     int? month,
+    int? index,
   }) {
     return CalendarLoaded(
       year: year ?? this.year,
       month: month ?? this.month,
+      index: index ?? this.index,
     );
   }
 }
