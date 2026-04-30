7fc30d666a12659886533ce379e2919408f8d18a
viren <virenrangani7@gmail.com>
2026-04-30 14:54:46 +0530

    feat: add monthScroll bar

--- lib/features/calendar/presentation/page/calendar_page.dart
+++ lib/features/calendar/presentation/page/calendar_page.dart
@@ -1,5 +1,6 @@
 import 'package:expenseo/core/constant/padding/app_padding.dart';
 import 'package:expenseo/features/calendar/presentation/cubit/calendar_cubit.dart';
+import 'package:expenseo/features/calendar/presentation/widget/month_scroll_bar.dart';
 import 'package:expenseo/features/calendar/presentation/widget/year_picker_popup.dart';
 import 'package:expenseo/features/expense/presentation/cubit/expense_cubit.dart';
 import 'package:flutter/material.dart';
@@ -28,31 +29,36 @@ class CalendarPage extends StatelessWidget {
           return Scaffold(
             backgroundColor: AppColor.primary,
             body: SafeArea(
-              child: Padding(
-                padding: AppPadding.edgeSymmetricHori24,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const YearPickerPopup(),
-                    IconButton(
-                        onPressed: () {
-                          final expenseCubit=context.read<ExpenseCubit>();
-                          showModalBottomSheet<void>(
-                            context: context,
-                            isScrollControlled: true,
-                            backgroundColor: Colors.transparent,
-                            builder: (_) =>
-                                BlocProvider.value(
-                                  value: expenseCubit,
-                                  child: const AddExpenseSheet(),
-                                ),
-                          );
-                        },
-                        icon: const Icon(
-                          Icons.add, size: 28, color: AppColor.background,)
-                    )
-                  ],
-                ),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: AppPadding.edgeSymmetricHori24,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const YearPickerPopup(),
+                        IconButton(
+                            onPressed: () {
+                              final expenseCubit=context.read<ExpenseCubit>();
+                              showModalBottomSheet<void>(
+                                context: context,
+                                isScrollControlled: true,
+                                backgroundColor: Colors.transparent,
+                                builder: (_) =>
+                                    BlocProvider.value(
+                                      value: expenseCubit,
+                                      child: const AddExpenseSheet(),
+                                    ),
+                              );
+                            },
+                            icon: const Icon(
+                              Icons.add, size: 28, color: AppColor.background,)
+                        )
+                      ],
+                    ),
+                  ),
+                    const MonthScrollBar()
+                ],
               ),
             ),
           );
