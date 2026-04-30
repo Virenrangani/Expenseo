3053fdedc29f7c3fdc97f9688da3356124f18647
viren <virenrangani7@gmail.com>
2026-04-30 11:54:15 +0530

    feat: provide calendar cubit and add year picker class

--- lib/features/calendar/presentation/page/calendar_page.dart
+++ lib/features/calendar/presentation/page/calendar_page.dart
@@ -1,11 +1,31 @@
-import 'package:flutter/cupertino.dart';
+import 'package:expenseo/core/constant/padding/app_padding.dart';
+import 'package:expenseo/features/calendar/presentation/cubit/calendar_cubit.dart';
+import 'package:expenseo/features/calendar/presentation/widget/year_picker_popup.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../core/constant/colour/app_color.dart';
 
 class CalendarPage extends StatelessWidget {
   const CalendarPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
-
+    return BlocProvider(
+      create: (_) => CalendarCubit(),
+      child: const Scaffold(
+        backgroundColor: AppColor.primary,
+        body: SafeArea(
+          child: Padding(
+            padding: AppPadding.edgeSymmetricHori24,
+            child: Row(
+              children: [
+                YearPickerPopup(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
