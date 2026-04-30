a0fcee5f5cb112000d1798f6bc54ec04543f9459
viren <virenrangani7@gmail.com>
2026-04-30 09:52:44 +0530

    feat: add calendar class

---
+++ lib/features/calendar/presentation/page/calendar_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class CalendarPage extends StatelessWidget {
+  const CalendarPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+
+  }
+}
