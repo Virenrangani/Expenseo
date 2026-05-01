bad4f8d2f97a1da67307a3db50b6187fa9c321cc
viren <virenrangani7@gmail.com>
2026-05-01 12:16:14 +0530

    feat: add day card showing the day correctly and identify with dim design and selected day with color design

---
+++ lib/features/calendar/presentation/widget/day_card.dart
@@ -0,0 +1,61 @@
+import 'package:expenseo/core/constant/border_radius/app_border_radius.dart';
+import 'package:expenseo/core/constant/text_style/app_text_style.dart';
+import 'package:flutter/material.dart';
+
+class DayCard extends StatelessWidget {
+
+
+  final int day;
+  final String letter;
+  final bool isActive;
+  final bool isDim;
+  final VoidCallback onTap;
+
+  const DayCard({super.key,
+    required this.day,
+    required this.letter,
+    required this.isActive,
+    required this.isDim,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          color: isActive ? const Color(0xFF6B6EF4) : Colors.transparent,
+          borderRadius: AppBorderRadius.cir20,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              letter,
+              style: AppTextStyles.caption(color: isActive
+              ? Colors.white70
+                  : isDim
+              ? Colors.white24
+                : Colors.white38,)
+            ),
+            const SizedBox(height: 4),
+            Text(
+              '$day',
+              style: TextStyle(
+                fontSize: 15,
+                fontWeight:
+                isActive ? FontWeight.bold : FontWeight.normal,
+                color: isActive
+                    ? Colors.white
+                    : isDim
+                    ? Colors.white24
+                    : Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
