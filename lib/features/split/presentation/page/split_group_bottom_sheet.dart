3af0feb27c8e90af8f9732a3e53741254aa35080
viren <virenrangani7@gmail.com>
2026-05-04 11:58:19 +0530

    feat: add split bottom sheet stateless class

---
+++ lib/features/split/presentation/page/split_group_bottom_sheet.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class SplitGroupBottomSheet extends StatelessWidget {
+  const SplitGroupBottomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
