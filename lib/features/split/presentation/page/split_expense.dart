7b1956b24837b09f6619e2bdc901e453b4d3b1d5
viren <virenrangani7@gmail.com>
2026-05-04 11:23:11 +0530

    feat: add title split your bill

--- lib/features/split/presentation/page/split_expense.dart
+++ lib/features/split/presentation/page/split_expense.dart
@@ -1,10 +1,28 @@
-import 'package:flutter/cupertino.dart';
+import 'package:expenseo/core/constant/colour/app_color.dart';
+import 'package:expenseo/core/constant/padding/app_padding.dart';
+import 'package:expenseo/core/constant/string/app_string.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../core/constant/text_style/app_text_style.dart';
 
 class SplitExpense extends StatelessWidget {
   const SplitExpense({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: AppColor.primaryLight,
+      body:SafeArea(
+          child: Padding(
+            padding: AppPadding.edgeAll12,
+            child: Column(
+              children: [
+                Text(AppString.splitBill,style: AppTextStyles.h4(color:AppColor.secondary),),
+
+              ],
+            ),
+          )
+      ),
+    );
   }
 }
