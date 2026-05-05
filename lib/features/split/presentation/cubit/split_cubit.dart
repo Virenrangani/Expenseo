47b63ca5163478fe5d2f82d19d3c6db053c0c4e6
viren <virenrangani7@gmail.com>
2026-05-05 11:34:07 +0530

    feat: add cubit state management

---
+++ lib/features/split/presentation/cubit/split_cubit.dart
@@ -0,0 +1,64 @@
+import 'package:expenseo/core/constant/string/app_string.dart';
+import 'package:expenseo/features/split/domain/entity/group_entity.dart';
+import 'package:expenseo/features/split/domain/use_case/split_use_case.dart';
+import 'package:expenseo/features/split/presentation/cubit/split_state.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:uuid/uuid.dart';
+
+class SplitCubit extends Cubit<SplitState> {
+  final SplitUseCase useCase;
+  SplitCubit(this.useCase) : super(SplitInitial());
+
+  String get currentUid  => FirebaseAuth.instance.currentUser!.uid;
+  String get currentName => FirebaseAuth.instance.currentUser!.displayName ?? '';
+
+  Future<void> searchUser(String email) async {
+    emit(UserSearchLoading());
+    try {
+      final user = await useCase.searchUserByEmail(email);
+
+      if (user == null) {
+        emit(UserSearchNotFound());
+        return;
+      }
+
+      if(user.uid==currentUid){
+        emit(SplitError(AppString.youCanNotAddYourSelf));
+        return;
+      }
+
+      emit(UserSearchResult(user: user));
+
+    } catch (e) {
+      emit(SplitError(e.toString()));
+    }
+  }
+
+  Future<void> createGroup({
+    required String name,
+    required List<String> memberUids,
+    required Map<String, String> memberNames,
+  }) async {
+    emit(SplitLoading());
+    try {
+      final allUids  = [currentUid, ...memberUids];
+      final allNames = {currentUid: currentName, ...memberNames};
+
+      final group = GroupEntity(
+        id: const Uuid().v4(),
+        name: name,
+        createdBy: currentUid,
+        members: allUids,
+        memberNames: allNames,
+        createdAt: DateTime.now(),
+      );
+
+      await useCase.createGroup(group);
+      emit(SplitSuccess(AppString.groupCreated));
+    } catch (e) {
+      emit(SplitError(e.toString()));
+    }
+  }
+
+}
---
+++ lib/features/split/presentation/cubit/split_state.dart
@@ -0,0 +1,28 @@
+import '../../domain/entity/user.dart';
+
+sealed class SplitState {}
+
+final class SplitInitial extends SplitState {}
+
+final class SplitLoading extends SplitState {}
+
+final class SplitSuccess extends SplitState {
+  final String message;
+  SplitSuccess(this.message);
+}
+
+final class SplitError extends SplitState {
+  final String message;
+  SplitError(this.message);
+}
+
+class UserSearchLoading extends SplitState {}
+
+class UserSearchResult extends SplitState {
+ final User user;
+  UserSearchResult({
+   required this.user
+  });
+}
+
+class UserSearchNotFound extends SplitState {}
