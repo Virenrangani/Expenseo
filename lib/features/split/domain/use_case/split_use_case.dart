858508d75a69195ef987c1288a0a1c4618ef2fb1
viren <virenrangani7@gmail.com>
2026-05-05 10:53:21 +0530

    feat: call the createGroup and searchUserByEmail method using repository

---
+++ lib/features/split/domain/use_case/split_use_case.dart
@@ -0,0 +1,17 @@
+import 'package:expenseo/features/split/domain/entity/group_entity.dart';
+import 'package:expenseo/features/split/domain/repository/split_repository.dart';
+
+import '../entity/user.dart';
+
+class SplitUseCase {
+  final SplitRepository repository;
+  SplitUseCase(this.repository);
+
+  Future<void> createGroup(GroupEntity group){
+    return repository.createGroup(group);
+  }
+
+  Future<User?> searchUserByEmail(String email) {
+    return repository.searchUserByEmail(email);
+  }
+}
