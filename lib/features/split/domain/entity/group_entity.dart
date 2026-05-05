0f2694dc5b50ba6e948f506a2cc24bdb41025ad4
viren <virenrangani7@gmail.com>
2026-05-05 10:49:41 +0530

    feat: add group entity and user entity

---
+++ lib/features/split/domain/entity/group_entity.dart
@@ -0,0 +1,17 @@
+class GroupEntity {
+  final String id;
+  final String name;
+  final String createdBy;
+  final List<String> members;
+  final Map<String, String> memberNames;
+  final DateTime createdAt;
+
+  const GroupEntity({
+    required this.id,
+    required this.name,
+    required this.createdBy,
+    required this.members,
+    required this.memberNames,
+    required this.createdAt,
+  });
+}
