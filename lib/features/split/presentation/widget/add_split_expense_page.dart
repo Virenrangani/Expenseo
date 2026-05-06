9375ac2f9edf03da7a89b4fa5fb3dd1ff90bdc53
viren <virenrangani7@gmail.com>
2026-05-06 10:49:32 +0530

    feat: add blocConsumer to listen and build all state

--- lib/features/split/presentation/widget/add_split_expense_page.dart
+++ lib/features/split/presentation/widget/add_split_expense_page.dart
@@ -1,4 +1,8 @@
+import 'package:expenseo/core/widget/snack_bar/custom_snack_bar.dart';
+import 'package:expenseo/features/split/presentation/cubit/split_cubit.dart';
+import 'package:expenseo/features/split/presentation/cubit/split_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../core/constant/colour/app_color.dart';
 import '../../../../core/constant/string/app_string.dart';
@@ -22,6 +26,23 @@ class _AddSplitExpensePageState extends State<AddSplitExpensePage> {
           onPressed: () => Navigator.pop(context),
         ),
         title: Text(AppString.addExpense, style: AppTextStyles.h5()),
+      ),
+      body:BlocConsumer<SplitCubit,SplitState>(
+
+        listener: (context,state){
+
+          if(state is SplitSuccess){
+            CustomSnacksBar.showSuccess(context,state.message);
+          }
+
+          if(state is SplitError){
+            CustomSnacksBar.showError(context, state.message);
+          }
+        },
+
+        builder: (context,state){
+          return const SizedBox.shrink();
+        }
       ),
     );
   }
