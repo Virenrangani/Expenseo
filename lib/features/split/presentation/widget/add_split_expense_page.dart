47c6e1ec49d76e7becd99927c2ce5dc2325604a6
viren <virenrangani7@gmail.com>
2026-05-06 10:10:55 +0530

    feat: add stateless class split expense page

---
+++ lib/features/split/presentation/widget/add_split_expense_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class AddSplitExpensePage extends StatefulWidget {
+  const AddSplitExpensePage({super.key});
+
+  @override
+  State<AddSplitExpensePage> createState() => _AddSplitExpensePageState();
+}
+
+class _AddSplitExpensePageState extends State<AddSplitExpensePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
