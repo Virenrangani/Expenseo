f09b4b78ba5a86f29f3ed485a634b3bf5cc96020
viren <virenrangani7@gmail.com>
2026-05-06 12:28:26 +0530

    feat: add PaidBySelector with parameter and add label

--- lib/features/split/presentation/widget/add_split_expense_page.dart
+++ lib/features/split/presentation/widget/add_split_expense_page.dart
@@ -1,6 +1,7 @@
 import 'package:expenseo/core/constant/gap/app_gap.dart';
 import 'package:expenseo/core/constant/padding/app_padding.dart';
 import 'package:expenseo/core/widget/snack_bar/custom_snack_bar.dart';
+import 'package:expenseo/features/split/domain/entity/group_entity.dart';
 import 'package:expenseo/features/split/presentation/cubit/split_cubit.dart';
 import 'package:expenseo/features/split/presentation/cubit/split_state.dart';
 import 'package:flutter/material.dart';
@@ -11,9 +12,12 @@ import '../../../../core/constant/string/app_string.dart';
 import '../../../../core/constant/text_style/app_text_style.dart';
 import '../../../../core/widget/amount_box/amount_box.dart';
 import '../../../../core/widget/text_field/app_text_field.dart';
+import 'add_split_expense/paid_by_selector.dart';
 
 class AddSplitExpensePage extends StatefulWidget {
-  const AddSplitExpensePage({super.key});
+  final String groupId;
+  final GroupEntity group;
+  const AddSplitExpensePage({super.key,required this.group, required this.groupId});
 
   @override
   State<AddSplitExpensePage> createState() => _AddSplitExpensePageState();
@@ -24,6 +28,9 @@ class _AddSplitExpensePageState extends State<AddSplitExpensePage> {
   final TextEditingController amountController = TextEditingController();
   final TextEditingController titleController = TextEditingController();
   final formKey=GlobalKey<FormState>();
+
+  String _paidByUid = '';
+  String _paidByName = '';
 
   @override
   Widget build(BuildContext context) {
@@ -67,7 +74,19 @@ class _AddSplitExpensePageState extends State<AddSplitExpensePage> {
                     validator:  (v) =>
                     v!.trim().isEmpty ? AppString.titleInvalid : null,
                   ),
-                  
+
+                  AppGap.g20,
+
+                  buildLabel(AppString.paidBy),
+                  AppGap.g8,
+                  PaidBySelector(
+                    group: widget.group,
+                    selectedUid: _paidByUid,
+                    onChanged: (uid, name) => setState(() {
+                      _paidByUid  = uid;
+                      _paidByName = name;
+                    }),
+                  ),
                 ],
               )
           );
