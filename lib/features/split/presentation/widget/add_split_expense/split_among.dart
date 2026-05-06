7eaecc54f07b926dfb53e75731ed976ec64d5117
viren <virenrangani7@gmail.com>
2026-05-06 14:28:50 +0530

    feat: add split among class split the expense according to user select splitType

---
+++ lib/features/split/presentation/widget/add_split_expense/split_among.dart
@@ -0,0 +1,115 @@
+import 'package:expenseo/core/constant/padding/app_padding.dart';
+import 'package:expenseo/features/split/domain/entity/group_entity.dart';
+import 'package:expenseo/features/split/domain/entity/split_entity.dart';
+import 'package:flutter/cupertino.dart';
+
+import '../../../../../core/constant/border_radius/app_border_radius.dart';
+import '../../../../../core/constant/colour/app_color.dart';
+import '../../../../../core/constant/gap/app_gap.dart';
+import '../../../../../core/constant/text_style/app_text_style.dart';
+import '../../../../../core/widget/text_field/app_text_field.dart';
+
+class SplitAmong extends StatefulWidget {
+  final GroupEntity group;
+  final SplitType splitType;
+  final TextEditingController amountController;
+
+  const SplitAmong({
+    super.key,
+    required this.group,
+    required this.splitType,
+    required this.amountController
+  });
+
+  @override
+  State<SplitAmong> createState() => _SplitAmongState();
+}
+
+class _SplitAmongState extends State<SplitAmong> {
+
+  final Map<String, TextEditingController> _splitControllers = {};
+  @override
+
+  void initState() {
+    super.initState();
+    for (final uid in widget.group.members) {
+      _splitControllers[uid] = TextEditingController();
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    for (final c in _splitControllers.values) {
+      c.dispose();
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+
+      children: widget.group.members.map((uid) {
+
+        final name = widget.group.memberNames[uid] ?? '';
+        final isEqual = widget.splitType == SplitType.equal;
+        final total = double.tryParse(widget.amountController.text) ?? 0;
+        final share = isEqual ? total / widget.group.members.length : null;
+
+        return Container(
+          margin:  const EdgeInsets.only(bottom: 8),
+          padding: AppPadding.edgeAll12,
+          decoration: BoxDecoration(
+            color: AppColor.background,
+            borderRadius: AppBorderRadius.cir12,
+            border: Border.all(
+                color: AppColor.divider.withAlpha(100)),
+          ),
+          child: Row(
+            children: [
+
+              Container(
+                width:  32,
+                height: 32,
+                decoration: BoxDecoration(
+                  color: AppColor.primary.withAlpha(40),
+                  shape: BoxShape.circle,
+                ),
+                child: Center(
+                  child: Text(
+                    name[0].toUpperCase(),
+                    style: AppTextStyles.captionMedium(
+                        color: AppColor.primary),
+                  ),
+                ),
+              ),
+              AppGap.g8,
+
+              Expanded(
+                child: Text(name,
+                    style: AppTextStyles.bodySmall(
+                        color: AppColor.textPrimary)),
+              ),
+
+              if (isEqual) Text(
+                '₹${share!.toStringAsFixed(0)}',
+                style: AppTextStyles.captionBold(),
+              ) else SizedBox(
+                width: 80,
+                child: AppFormField(
+                  controller: _splitControllers[uid],
+                  hintText:  widget.splitType == SplitType.percentage
+                      ? '0%'
+                      : '₹0',
+                  keyboardType: const TextInputType.numberWithOptions(
+                      decimal: true),
+                  textAlign: TextAlign.center,
+                  onChanged: (_) => setState(() {}),
+                ),
+              ),
+            ],
+          ),
+        );
+      }).toList(),
+    );
+  }
+}
