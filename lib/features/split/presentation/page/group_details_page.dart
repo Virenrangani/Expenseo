0ccea594965480a3b587c918d7995b09ee09eef3
viren <virenrangani7@gmail.com>
2026-05-06 09:34:24 +0530

    feat: add stateful group details class

---
+++ lib/features/split/presentation/page/group_details_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class GroupDetailsPage extends StatefulWidget {
+  const GroupDetailsPage({super.key});
+
+  @override
+  State<GroupDetailsPage> createState() => _GroupDetailsPageState();
+}
+
+class _GroupDetailsPageState extends State<GroupDetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
