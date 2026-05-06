7317fc7c4810ad10831dd72f04b9736aee4234f2
viren <virenrangani7@gmail.com>
2026-05-06 10:33:45 +0530

    feat: add appbar with iconButton and title

--- lib/features/split/presentation/widget/add_split_expense_page.dart
+++ lib/features/split/presentation/widget/add_split_expense_page.dart
@@ -1,4 +1,8 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../core/constant/colour/app_color.dart';
+import '../../../../core/constant/string/app_string.dart';
+import '../../../../core/constant/text_style/app_text_style.dart';
 
 class AddSplitExpensePage extends StatefulWidget {
   const AddSplitExpensePage({super.key});
@@ -10,6 +14,15 @@ class AddSplitExpensePage extends StatefulWidget {
 class _AddSplitExpensePageState extends State<AddSplitExpensePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColor.background,
+        leading: IconButton(
+          icon:const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.pop(context),
+        ),
+        title: Text(AppString.addExpense, style: AppTextStyles.h5()),
+      ),
+    );
   }
 }
