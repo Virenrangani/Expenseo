64bbc57834ca2a7833b2acc63e06b56e29220da3
viren <virenrangani7@gmail.com>
2026-05-06 09:52:31 +0530

    feat: add blocConsumer to listen and build all state

--- lib/features/split/presentation/page/group_details_page.dart
+++ lib/features/split/presentation/page/group_details_page.dart
@@ -1,4 +1,9 @@
-import 'package:flutter/cupertino.dart';
+import 'package:expenseo/core/constant/colour/app_color.dart';
+import 'package:expenseo/core/widget/snack_bar/custom_snack_bar.dart';
+import 'package:expenseo/features/split/presentation/cubit/split_cubit.dart';
+import 'package:expenseo/features/split/presentation/cubit/split_state.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class GroupDetailsPage extends StatefulWidget {
   const GroupDetailsPage({super.key});
@@ -10,6 +15,29 @@ class GroupDetailsPage extends StatefulWidget {
 class _GroupDetailsPageState extends State<GroupDetailsPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: AppColor.primaryLight,
+      body:BlocConsumer<SplitCubit,SplitState>(
+          listener:(context,state){
+
+            if(state is SplitSuccess){
+              CustomSnacksBar.showSuccess(context, state.message);
+            }
+
+            if(state is SplitError){
+              CustomSnacksBar.showError(context, state.message);
+            }
+          },
+          builder: (context,state){
+
+            if(state is SplitLoading){
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            return const SizedBox.shrink();
+          }
+      ),
+    );
   }
 }
