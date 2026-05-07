9bc43a59ca11fe07135802c3b4afc33c68c81baf
viren <virenrangani7@gmail.com>
2026-05-07 17:23:22 +0530

    feat: add label and ExpenseSection class with list of expenses

--- lib/features/split/presentation/widget/group_details_view.dart
+++ lib/features/split/presentation/widget/group_details_view.dart
@@ -2,12 +2,14 @@ import 'package:expenseo/core/constant/gap/app_gap.dart';
 import 'package:expenseo/core/constant/padding/app_padding.dart';
 import 'package:expenseo/core/constant/string/app_string.dart';
 import 'package:expenseo/features/split/presentation/cubit/split_state.dart';
+import 'package:expenseo/features/split/presentation/widget/empty_state.dart';
 import 'package:expenseo/features/split/presentation/widget/group_details_view/member_row.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../core/constant/colour/app_color.dart';
 import '../../../../core/constant/text_style/app_text_style.dart';
+import 'group_details_view/expense_section.dart';
 import 'group_details_view/balance_card.dart';
 import 'group_details_view/settled_card.dart';
 import 'group_details_view/show_settle_dialog.dart';
@@ -62,6 +64,21 @@ class GroupDetailsView extends StatelessWidget {
                   ),
                 );
               }),
+
+              AppGap.g20,
+
+              Text(AppString.expense,
+                  style: AppTextStyles.captionBold(
+                      color: AppColor.textPrimary
+                  )
+              ),
+
+              AppGap.g8,
+
+              if (state.expenses.isEmpty)
+                const EmptyState()
+              else
+                ExpenseSection(expenses: state.expenses)
             ],
           )
         )
