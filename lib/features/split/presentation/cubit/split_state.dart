0393c74d34221ebd3a5aad68ff6b78c53a7d66c1
viren <virenrangani7@gmail.com>
2026-05-07 17:18:32 +0530

    feat: add GroupDetails loaded state with mwthod calculateBalances

--- lib/features/split/presentation/cubit/split_state.dart
+++ lib/features/split/presentation/cubit/split_state.dart
@@ -1,5 +1,6 @@
 import 'package:expenseo/features/split/domain/entity/group_entity.dart';
 
+import '../../domain/entity/split_entity.dart';
 import '../../domain/entity/user.dart';
 
 sealed class SplitState {}
@@ -21,6 +22,36 @@ final class SplitLoaded extends SplitState{
 final class SplitError extends SplitState {
   final String message;
   SplitError(this.message);
+}
+
+class GroupDetailLoaded extends SplitState {
+  final GroupEntity group;
+  final List<SplitEntity> expenses;
+
+  GroupDetailLoaded({
+    required this.group,
+    required this.expenses,
+  });
+
+  Map<String, double> calculateBalances(String currentUid) {
+    final Map<String, double> balances = {};
+
+    for (final expense in expenses) {
+      if (expense.paidBy == currentUid) {
+        expense.splitAmong.forEach((uid, share) {
+          if (uid != currentUid) {
+            balances[uid] = (balances[uid] ?? 0) + share;
+          }
+        });
+      } else {
+        final myShare = expense.splitAmong[currentUid] ?? 0;
+        if (myShare > 0) {
+          balances[expense.paidBy] = (balances[expense.paidBy] ?? 0) - myShare;
+        }
+      }
+    }
+    return balances;
+  }
 }
 
 class UserSearchLoading extends SplitState {}
