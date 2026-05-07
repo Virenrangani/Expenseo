5b9ca5a4c410301899bc534609364c83ac3905ab
viren <virenrangani7@gmail.com>
2026-05-07 10:43:30 +0530

    feat: add GroupDetailsView class

---
+++ lib/features/split/presentation/widget/group_details_view.dart
@@ -0,0 +1,12 @@
+import 'package:expenseo/features/split/presentation/cubit/split_state.dart';
+import 'package:flutter/cupertino.dart';
+
+class GroupDetailsView extends StatelessWidget {
+  final GroupDetailLoaded state;
+  const GroupDetailsView({super.key, required this.state});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
