d67e2b129e27996c834f7638a8f26441da17f694
viren <virenrangani7@gmail.com>
2026-05-08 17:33:18 +0530

    feat: add group id as parameter in showSettleDialog

--- lib/features/split/presentation/widget/group_details_view.dart
+++ lib/features/split/presentation/widget/group_details_view.dart
@@ -58,6 +58,7 @@ class GroupDetailsView extends StatelessWidget {
                   groupId: state.group.id,
                   onSettle: () => showSettleDialog(
                     context: context,
+                    groupId:state.group.id,
                     uid: entry.key,
                     name: memberName,
                     amount: entry.value,
