931404b2834d4e37e2c8a6e00022a3912b6de780
viren <virenrangani7@gmail.com>
2026-05-11 15:10:11 +0530

    refactor: change the log in title and subtitle

--- lib/features/auth/presentation/widget/log_in_title.dart
+++ lib/features/auth/presentation/widget/log_in_title.dart
@@ -2,7 +2,8 @@ import 'package:expenseo/core/constant/string/app_string.dart';
 import 'package:expenseo/core/constant/text_style/app_text_style.dart';
 import 'package:flutter/cupertino.dart';
 
-import '../../../../core/widget/app_title/app_title.dart';
+import '../../../../core/constant/colour/app_color.dart';
+import '../../../../core/constant/gap/app_gap.dart';
 
 class LogInTitle extends StatelessWidget {
   const LogInTitle({super.key});
@@ -12,8 +13,22 @@ class LogInTitle extends StatelessWidget {
     return Center(
       child: Column(
         children: [
-          const AppTitle(),
-          Text(AppString.appIntro, style: AppTextStyles.captionMedium()),
+          Text(
+            AppString.logInIntro,
+            textAlign: TextAlign.center,
+            style: AppTextStyles.h1(
+              color: AppColor.background,
+            ),
+          ),
+
+          AppGap.g12,
+
+          Text(
+            AppString.logInSubIntro,
+            style: AppTextStyles.bodySmall(
+              color: AppColor.background,
+            ),
+          ),
         ],
       ),
     );
