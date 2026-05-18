3868fa51ba637cc727a87c68b2dd0958eef50f2a
viren <virenrangani7@gmail.com>
2026-05-18 11:46:45 +0530

    refactor: change ExpenseContainer color

--- lib/features/home/presentation/widget/expense_container.dart
+++ lib/features/home/presentation/widget/expense_container.dart
@@ -54,7 +54,7 @@ class _ExpenseContainerState extends State<ExpenseContainer> {
       width: double.infinity,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(20),
-        color: AppColor.primary,
+        color: AppColor.background.withAlpha(30),
       ),
       padding: AppPadding.edgeAll12,
       child: Column(
