89930c890ee3d148b1f45392b755c447932539a4
viren <virenrangani7@gmail.com>
2026-05-19 10:41:58 +0530

    feat: add AppLotty class and add path of the lottie images

---
+++ lib/core/constant/lotties/app_lottie.dart
@@ -0,0 +1,6 @@
+class AppLottie {
+  static const String home='assets/lotties/Home.json';
+  static const String graph='assets/lotties/Graph.json';
+  static const String profile='assets/lotties/Profile.json';
+
+}
