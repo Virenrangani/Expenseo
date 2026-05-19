e4d3e685bcb0097c58fd90089194aee343353f66
viren <virenrangani7@gmail.com>
2026-05-19 17:24:21 +0530

    refactor: remove unnecessary container

--- lib/features/expense/presentation/page/add_expense_sheet.dart
+++ lib/features/expense/presentation/page/add_expense_sheet.dart
@@ -1,14 +1,9 @@
-import 'package:expenseo/core/validation/amount_validation/amount_validation.dart';
 import 'package:expenseo/core/widget/snack_bar/custom_snack_bar.dart';
 import 'package:expenseo/features/expense/presentation/widget/amount_field.dart';
-import 'package:expenseo/features/expense/presentation/widget/center_line_design.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:uuid/uuid.dart';
-import '../../../../core/constant/border_radius/app_border_radius.dart';
-import '../../../../core/constant/colour/app_color.dart';
 import '../../../../core/constant/gap/app_gap.dart';
-import '../../../../core/constant/padding/app_padding.dart';
 import '../../../../core/constant/string/app_string.dart';
 import '../../../../core/constant/text_style/app_text_style.dart';
 import '../../../../core/enums/app_enums.dart';
@@ -48,69 +43,59 @@ class _AddExpenseSheetState extends State<AddExpenseSheet> {
             CustomSnacksBar.showSuccess(context, state.message);
           }
         },
-        child: Container(
-          decoration: BoxDecoration(
-            color: AppColor.background,
-            borderRadius: AppBorderRadius.verTop24,
-          ),
-          padding: AppPadding.edgeAll16,
-          child: SingleChildScrollView(
-            child: Form(
-              key: _formKey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const CenterLineDesign(),
-                  AppGap.g8,
-
-                  Center(
-                    child: Text(
-                      context.read<ExpenseCubit>().type == TransactionType.expense ? AppString.addExpense : AppString.addIncome,
-                      style: AppTextStyles.h5(),
-                    ),
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Center(
+                  child: Text(
+                    context.read<ExpenseCubit>().type == TransactionType.expense ? AppString.addExpense : AppString.addIncome,
+                    style: AppTextStyles.h5(),
                   ),
-                  AppGap.g16,
+                ),
+                AppGap.g16,
 
-                  AmountField(controller: amountController),
-                  AppGap.g16,
+                AmountField(controller: amountController),
+                AppGap.g16,
 
-                  ExpenseTypeToggle(
-                    selectedType:context.read<ExpenseCubit>().type,
-                    onChanged: (typeText) => setState(() => context.read<ExpenseCubit>().type = typeText),
-                  ),
-                  AppGap.g16,
+                ExpenseTypeToggle(
+                  selectedType:context.read<ExpenseCubit>().type,
+                  onChanged: (typeText) => setState(() => context.read<ExpenseCubit>().type = typeText),
+                ),
+                AppGap.g16,
 
-                  sectionLabel(AppString.title),
-                  AppGap.g8,
-                  AppFormField(
-                    controller: titleController,
-                    hintText: AppString.titleHint  ,
-                    validator:  (v) => v!.trim().isEmpty ? AppString.titleInvalid : null,
-                  ),
+                sectionLabel(AppString.title),
+                AppGap.g8,
+                AppFormField(
+                  controller: titleController,
+                  hintText: AppString.titleHint  ,
+                  validator:  (v) => v!.trim().isEmpty ? AppString.titleInvalid : null,
+                ),
 
-                  AppGap.g16,
-                  sectionLabel(AppString.category),
-                  AppGap.g8,
-                  ExpenseCategorySelector(
-                    selectedCategory: context.read<ExpenseCubit>().category,
-                    onChanged: (cat) => setState(() => context.read<ExpenseCubit>().category = cat),
-                  ),
+                AppGap.g16,
+                sectionLabel(AppString.category),
+                AppGap.g8,
+                ExpenseCategorySelector(
+                  selectedCategory: context.read<ExpenseCubit>().category,
+                  onChanged: (cat) => setState(() => context.read<ExpenseCubit>().category = cat),
+                ),
 
-                  AppGap.g16,
-                  sectionLabel(AppString.paymentMethod),
-                  AppGap.g8,
-                  PaymentMethodSelector(
-                    selectedMethod: context.read<ExpenseCubit>().paymentMethod,
-                    onChanged: (method) => setState(() => context.read<ExpenseCubit>().paymentMethod = method),
-                  ),
-                  AppGap.g20,
-                  submitButton(),
-                ],
-              ),
+                AppGap.g16,
+                sectionLabel(AppString.paymentMethod),
+                AppGap.g8,
+                PaymentMethodSelector(
+                  selectedMethod: context.read<ExpenseCubit>().paymentMethod,
+                  onChanged: (method) => setState(() => context.read<ExpenseCubit>().paymentMethod = method),
+                ),
+                AppGap.g20,
+                submitButton(),
+              ],
             ),
           ),
-    ),
+        ),
     );
   }
 
