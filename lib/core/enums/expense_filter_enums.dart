6750ed7c53e2eb3064aff57dedaefcf45458ec6a
viren <virenrangani7@gmail.com>
2026-05-21 09:29:45 +0530

    feat: add expense filter enums

---
+++ lib/core/enums/expense_filter_enums.dart
@@ -0,0 +1,32 @@
+enum DateFilter {
+  all,
+  today,
+  week,
+  month,
+}
+
+enum TypeFilter {
+  all,
+  income,
+  expense,
+}
+
+enum CategoryFilter {
+  all,
+  ood,
+  shopping,
+  transport,
+  health,
+  entertainment,
+  salary,
+  rent,
+  other
+}
+
+enum PaymentType {
+  all,
+  cash,
+  upi,
+  card,
+  netbanking
+}
