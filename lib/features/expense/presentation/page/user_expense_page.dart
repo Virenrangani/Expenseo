cc67420124259215cd6c7cf3607fcced4d4be955
viren <virenrangani7@gmail.com>
2026-05-21 13:28:07 +0530

    fear: add search field and FilterButton

--- lib/features/expense/presentation/page/user_expense_page.dart
+++ lib/features/expense/presentation/page/user_expense_page.dart
@@ -1,16 +1,22 @@
+import 'package:expenseo/core/constant/border_radius/app_border_radius.dart';
+import 'package:expenseo/core/constant/gap/app_gap.dart';
 import 'package:expenseo/core/constant/padding/app_padding.dart';
 import 'package:expenseo/core/constant/string/app_string.dart';
+import 'package:expenseo/core/widget/text_field/app_text_field.dart';
 import 'package:expenseo/features/expense/presentation/cubit/expense_state.dart';
 import 'package:expenseo/features/expense/presentation/widget/expense_card.dart';
 import 'package:expenseo/features/expense/presentation/widget/fake_expense.dart';
+import 'package:expenseo/features/expense/presentation/widget/filter_button.dart';
 import 'package:expenseo/features/expense/presentation/widget/scroll_tab_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:skeletonizer/skeletonizer.dart';
 
 import '../../../../core/constant/colour/app_color.dart';
 import '../../../../core/constant/text_style/app_text_style.dart';
 import '../cubit/expense_cubit.dart';
+import '../widget/filter_dailog.dart';
 import './add_expense_sheet.dart';
 import 'expense_list_page.dart';
 
@@ -31,18 +37,23 @@ class UserExpensePage extends StatelessWidget {
           Column(
             children: [
               Padding(
-                padding: AppPadding.edgeSymmetricVer12,
-                child: ScrollTabBar(
-
-                  onFilterChanged: (dateFilter, typeFilter, categoryFilter, paymentFilter) {
-                    context.read<ExpenseCubit>().applyFilters(
-                      dateFilter: dateFilter,
-                      typeFilter: typeFilter,
-                      categoryFilter: categoryFilter,
-                      paymentFilter: paymentFilter,
-                    );
-                  },
-                )
+                padding: AppPadding.edgeSymmetricHori16,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: SizedBox(
+                        height: 48,
+                        child: AppFormField(
+                          hintText: 'Search your Expense..',
+                          fillColor: AppColor.primary.withAlpha(30),
+                          prefixIcon: const Icon(Icons.search_rounded , color: AppColor.textSecondary,),
+                        ),
+                      ),
+                    ),
+                    AppGap.g8,
+                    const FilterButton()
+                  ],
+                ),
               ),
 
               Expanded(
@@ -70,9 +81,10 @@ class UserExpensePage extends StatelessWidget {
                                   ),
                                   ...List.generate(
                                     6,
-                                    (_) => ExpenseCard(
-                                      expense: FakeExpense.fake(),
-                                    ),
+                                        (_) =>
+                                        ExpenseCard(
+                                          expense: FakeExpense.fake(),
+                                        ),
                                   ),
                                   const SizedBox(height: 16),
                                 ],
@@ -98,29 +110,30 @@ class UserExpensePage extends StatelessWidget {
             right: 20,
             bottom: 30,
             child: Builder(
-              builder: (context) {
-                return FloatingActionButton(
-                  backgroundColor: AppColor.primary,
-                  onPressed: () {
-                    final expenseCubit = context.read<ExpenseCubit>();
-                    showBottomSheet(
-                      context: context,
-                      builder: (_) => BlocProvider.value(
-                        value: expenseCubit,
-                        child: const AddExpenseSheet(),
-                      ),
-                      enableDrag: true,
-                      showDragHandle: true,
-                      backgroundColor: AppColor.background,
-                    );
-                  },
-                  child: const Icon(
-                    Icons.add_circle_outline,
-                    size: 32,
-                    color: AppColor.background,
-                  ),
-                );
-              }
+                builder: (context) {
+                  return FloatingActionButton(
+                    backgroundColor: AppColor.primary,
+                    onPressed: () {
+                      final expenseCubit = context.read<ExpenseCubit>();
+                      showBottomSheet(
+                        context: context,
+                        builder: (_) =>
+                            BlocProvider.value(
+                              value: expenseCubit,
+                              child: const AddExpenseSheet(),
+                            ),
+                        enableDrag: true,
+                        showDragHandle: true,
+                        backgroundColor: AppColor.background,
+                      );
+                    },
+                    child: const Icon(
+                      Icons.add_circle_outline,
+                      size: 32,
+                      color: AppColor.background,
+                    ),
+                  );
+                }
             ),
           ),
         ],
