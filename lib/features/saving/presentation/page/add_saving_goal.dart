21e2541157e3ecd95f8ae41d432d05c19ffa0bb3
viren <virenrangani7@gmail.com>
2026-05-25 15:34:52 +0530

    feat: add bloc consumer ,  listen and builder the state

--- lib/features/saving/presentation/page/add_saving_goal.dart
+++ lib/features/saving/presentation/page/add_saving_goal.dart
@@ -4,7 +4,9 @@ import 'package:expenseo/core/constant/padding/app_padding.dart';
 import 'package:expenseo/core/constant/string/app_string.dart';
 import 'package:expenseo/core/constant/text_style/app_text_style.dart';
 import 'package:expenseo/core/widget/elevated_button/app_elevated_button.dart';
+import 'package:expenseo/core/widget/snack_bar/custom_snack_bar.dart';
 import 'package:expenseo/core/widget/text_field/app_text_field.dart';
+import 'package:expenseo/features/saving/presentation/cubit/saving_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -24,63 +26,97 @@ class _AddSavingGoalState extends State<AddSavingGoal> {
 
   @override
   Widget build(BuildContext context) {
+    return BlocConsumer<SavingCubit, SavingState>(
+  listener: (context, state) {
+
+    if(state is SavingSuccess){
+      CustomSnacksBar.showSuccess(context, state.message);
+    }
+
+    if(state is SavingError){
+      CustomSnacksBar.showError(context, state.message);
+    }
+  },
+
+  builder: (context, state) {
+    final isLoading = state is SavingLoading;
+
     return Padding(
-      padding: AppPadding.edgeSymmetricHori24,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Center(
-            child: Text(
-              AppString.newSavingGoal,
-              style: AppTextStyles.h5(),
-            ),
-          ),
-
-          Text(AppString.goal,style: AppTextStyles.caption(color: AppColor.textPrimary)),
-          AppGap.g4,
-          AppFormField(
-            controller: goalController,
-            hintText: AppString.goalHint,
-            prefixIcon: const Icon(Icons.savings_outlined ,color: AppColor.textSecondary,),
-            fillColor: AppColor.primaryLight.withAlpha(50),
-            validator:  (v) => v!.trim().isEmpty ? 'Title is required' : null,
-          ),
-
-          AppGap.g20,
-
-          Row(
+      padding: EdgeInsets.only(
+        left: 16, right: 16, top: 16,
+        bottom: MediaQuery.of(context).viewInsets.bottom + 24,
+      ),
+      child: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
             children: [
-              Text(AppString.targetAmount,style: AppTextStyles.caption(color: AppColor.textPrimary)),
-              AppGap.g8,
-              Expanded(
-                child: AppFormField(
-                  controller: targetAmountController,
-                  hintText: AppString.targetAmountGoal,
-                  prefixText: '₹ ',
-                  fillColor: AppColor.primaryLight.withAlpha(50),
-                  keyboardType: const TextInputType.numberWithOptions(decimal: true),
-                  textAction: TextInputAction.done,
+              Center(
+                child: Text(
+                  AppString.newSavingGoal,
+                  style: AppTextStyles.h5(),
                 ),
               ),
+          
+              Text(AppString.goal,style: AppTextStyles.caption(color: AppColor.textPrimary)),
+              AppGap.g4,
+              AppFormField(
+                controller: goalController,
+                hintText: AppString.goalHint,
+                prefixIcon: const Icon(Icons.savings_outlined ,color: AppColor.textSecondary,),
+                fillColor: AppColor.primaryLight.withAlpha(50),
+                validator:  (value) {
+                  return context.read<SavingCubit>().validateGoal(value ?? '');
+                },
+              ),
+          
+              AppGap.g20,
+          
+              Row(
+                children: [
+                  Text(AppString.targetAmount,style: AppTextStyles.caption(color: AppColor.textPrimary)),
+                  AppGap.g8,
+                  Expanded(
+                    child: AppFormField(
+                      controller: targetAmountController,
+                      hintText: AppString.targetAmountGoal,
+                      prefixText: '₹ ',
+                      fillColor: AppColor.primaryLight.withAlpha(50),
+                      keyboardType: const TextInputType.numberWithOptions(decimal: true),
+                      textAction: TextInputAction.done,
+                      validator:  (value) {
+                        return context.read<SavingCubit>().validateTargetAmount(value ?? '');
+                      },
+                    ),
+                  ),
+                ],
+              ),
+          
+              AppGap.g20,
+          
+              AppElevatedButton(
+                  text: AppString.addGoal,
+                  onPressed: onSubmit,
+                isLoading: isLoading,
+                isEnabled: true,
+              )
             ],
           ),
-
-          AppGap.g20,
-
-          AppElevatedButton(
-              text: AppString.addGoal,
-              onPressed: onSubmit,
-            isEnabled: true,
-          )
-        ],
+        ),
       ),
     );
+  },
+);
   }
-  void onSubmit() {
+
+  Future<void> onSubmit()async {
     if (!_formKey.currentState!.validate()) return;
-    context.read<SavingCubit>().createGoal(
+    await context.read<SavingCubit>().createGoal(
       goal: goalController.text.trim(),
       targetAmount: double.parse(targetAmountController.text.trim()),
     );
+    Navigator.pop(context);
   }
 }
