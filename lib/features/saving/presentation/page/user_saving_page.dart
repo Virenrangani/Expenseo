d16a2f5c35c538eddd3f871691442e2d66b6fe7a
viren <virenrangani7@gmail.com>
2026-05-25 09:22:38 +0530

    feat: add user saving page class

---
+++ lib/features/saving/presentation/page/user_saving_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class UserSavingPage extends StatelessWidget {
+  const UserSavingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
