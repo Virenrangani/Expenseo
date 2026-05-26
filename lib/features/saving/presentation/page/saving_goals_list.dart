0a3d5376cf0d9f34e3dabaf315b02fa3657dcb26
viren <virenrangani7@gmail.com>
2026-05-26 13:22:39 +0530

    fix: remove unused import

--- lib/features/saving/presentation/page/saving_goals_list.dart
+++ lib/features/saving/presentation/page/saving_goals_list.dart
@@ -1,5 +1,4 @@
 import 'package:carousel_slider/carousel_slider.dart';
-import 'package:expenseo/core/constant/padding/app_padding.dart';
 import 'package:flutter/cupertino.dart';
 
 import '../../domain/entity/saving_goal.dart';
