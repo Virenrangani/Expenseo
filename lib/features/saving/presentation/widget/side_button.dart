48d83d7d994e8b199703c825ab762f1f77eb8617
viren <virenrangani7@gmail.com>
2026-05-26 17:22:46 +0530

    feat: add inkwell and parameter void callback

--- lib/features/saving/presentation/widget/side_button.dart
+++ lib/features/saving/presentation/widget/side_button.dart
@@ -5,24 +5,32 @@ import '../../../../core/constant/padding/app_padding.dart';
 
 class SideButton extends StatelessWidget {
   final IconData iconData;
-  const SideButton({super.key, required this.iconData});
+  final VoidCallback? onTap;
+  const SideButton({
+    super.key,
+    required this.iconData,
+    this.onTap
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: AppPadding.edgeAll8,
-        child: Container(
-          height: 68,
-          width: 68,
-          decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              border: Border.all(
-                  width: 2,
-                  color: AppColor.textLight.withAlpha(100)
-              )
-          ),
-          child: Icon(iconData , size: 32 , color: AppColor.background),
-        )
+    return InkWell(
+      onTap: onTap,
+      child: Padding(
+          padding: AppPadding.edgeAll8,
+          child: Container(
+            height: 68,
+            width: 68,
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                    width: 2,
+                    color: AppColor.textLight.withAlpha(100)
+                )
+            ),
+            child: Icon(iconData , size: 32 , color: AppColor.background),
+          )
+      ),
     );
   }
 }
