b8efad30128099e0b809d45640a0ec47b6f4878a
viren <virenrangani7@gmail.com>
2026-05-26 14:19:24 +0530

    feat: add bloc builder and build the all state

--- lib/features/saving/presentation/page/user_saving_page.dart
+++ lib/features/saving/presentation/page/user_saving_page.dart
@@ -1,11 +1,14 @@
 import 'package:expenseo/features/saving/presentation/cubit/saving_cubit.dart';
+import 'package:expenseo/features/saving/presentation/cubit/saving_state.dart';
 import 'package:expenseo/features/saving/presentation/page/add_saving_goal.dart';
+import 'package:expenseo/features/saving/presentation/page/saving_goals_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 
 import '../../../../core/constant/colour/app_color.dart';
 import '../../../../core/constant/text_style/app_text_style.dart';
+
 
 class UserSavingPage extends StatelessWidget {
   const UserSavingPage({super.key});
@@ -18,7 +21,24 @@ class UserSavingPage extends StatelessWidget {
         appBar: AppBar(
           title: Text('Savings', style: AppTextStyles.h4()),
         ),
-        body: Container(),
+        body: BlocBuilder<SavingCubit,SavingState>(
+            builder: (context , state){
+              if(state is SavingLoading){
+                return const Center(child: CircularProgressIndicator(),);
+              }
+
+              if(state is SavingError){
+                return Center(child: Text(state.message),);
+              }
+
+              if(state is SavingLoaded){
+                return SavingGoalsList(savingGoals: state.goals);
+              }
+
+              return const SizedBox.shrink();
+            }
+        ),
+
         floatingActionButton: Builder(
           builder: (context) {
             return FloatingActionButton(
