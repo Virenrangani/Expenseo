1896bea26ad60445df3c5fc65cd08c5d640dfd18
viren <virenrangani7@gmail.com>
2026-05-27 11:29:33 +0530

    feat: add goal details class

---
+++ lib/features/saving/presentation/page/goal_detail_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class GoalDetailPage extends StatelessWidget {
+  const GoalDetailPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
