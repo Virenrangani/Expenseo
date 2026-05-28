0a29dd8a2f8b32876515b9fc6a8dbaa85f90dbe1
viren <virenrangani7@gmail.com>
2026-05-28 13:57:23 +0530

    feat: add Draggable scrollable sheet

--- lib/features/saving/presentation/widget/deposit_list.dart
+++ lib/features/saving/presentation/widget/deposit_list.dart
@@ -1,12 +1,13 @@
-import 'package:expenseo/core/constant/gap/app_gap.dart';
-import 'package:expenseo/core/constant/padding/app_padding.dart';
-import 'package:expenseo/features/saving/domain/entity/saving_goal.dart';
-import 'package:expenseo/features/saving/presentation/widget/deposit_tile.dart';
-import 'package:expenseo/features/saving/presentation/widget/showing_goal_data.dart';
+import 'package:expenseo/core/constant/border_radius/app_border_radius.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../core/constant/colour/app_color.dart';
+import '../../../../core/constant/gap/app_gap.dart';
+import '../../../../core/constant/padding/app_padding.dart';
 import '../../../../core/constant/text_style/app_text_style.dart';
 import '../../domain/entity/deposit.dart';
+import '../../domain/entity/saving_goal.dart';
+import 'deposit_tile.dart';
 
 class DepositList extends StatelessWidget {
   final List<Deposit> deposits;
@@ -16,25 +17,120 @@ class DepositList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ShowingGoalData(goal: goal),
-        AppGap.g20,
-        Padding(
-          padding: AppPadding.edgeSymmetricHori16,
-          child: Text('Deposit History', style: AppTextStyles.caption()),
-        ),
-        Expanded(
-          child: ListView.builder(
-            padding: EdgeInsets.zero,
-            itemCount: deposits.length,
-            itemBuilder: (context, index) {
-              final deposit = deposits[index];
-              return DepositTile(deposit: deposit);
+    return Scaffold(
+      backgroundColor: Colors.black,
+
+      body: Stack(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.45,
+            width: double.infinity,
+
+            child: Hero(
+              tag: goal.id,
+
+              child: ShaderMask(
+                shaderCallback: (bounds) {
+                  return LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+
+                    colors: [Colors.black.withAlpha(200), Colors.transparent],
+                  ).createShader(bounds);
+                },
+                blendMode: BlendMode.darken,
+                child: Image.network(goal.goalImage, fit: BoxFit.cover),
+              ),
+            ),
+          ),
+
+          Positioned(
+            top: 42,
+            left: 8,
+            child: Row(
+              children: [
+                IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: const Icon(
+                    Icons.arrow_back,
+                    color: AppColor.background,
+                  ),
+                ),
+                Text(
+                  'Goal Details',
+                  style: AppTextStyles.h4(color: AppColor.background),
+                ),
+              ],
+            ),
+          ),
+
+          /// DRAGGABLE SHEET
+          DraggableScrollableSheet(
+            initialChildSize: 0.55,
+            minChildSize: 0.55,
+            maxChildSize: 0.86,
+            snap: true,
+            snapSizes: const [0.55, 0.86],
+            builder: (context, scrollController) {
+              return Container(
+                decoration: BoxDecoration(
+                  color: AppColor.background,
+                  borderRadius: AppBorderRadius.cir24,
+                ),
+
+                child: Column(
+                  children: [
+                    AppGap.g12,
+
+                    Container(
+                      width: 60,
+                      height: 6,
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+
+                    AppGap.g12,
+
+                    Padding(
+                      padding: AppPadding.edgeSymmetricHori16,
+
+                      child: Column(
+                        children: [
+                          Text(
+                            'Deposit History',
+                            style: AppTextStyles.captionBold(),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    AppGap.g12,
+
+                    Expanded(
+                      child: ListView.builder(
+                        padding: EdgeInsets.zero,
+                        controller: scrollController,
+
+                        physics: const BouncingScrollPhysics(),
+
+                        itemCount: deposits.length,
+
+                        itemBuilder: (context, index) {
+                          return DepositTile(deposit: deposits[index]);
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              );
             },
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
