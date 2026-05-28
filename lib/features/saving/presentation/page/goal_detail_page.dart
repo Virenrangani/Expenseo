846b6c47c51b1de4b09c5e7036c03daf6adde7c3
viren <virenrangani7@gmail.com>
2026-05-28 10:20:41 +0530

    feat: add parameter SavingGoal goal

--- lib/features/saving/presentation/page/goal_detail_page.dart
+++ lib/features/saving/presentation/page/goal_detail_page.dart
@@ -1,4 +1,4 @@
-import 'package:expenseo/core/constant/text_style/app_text_style.dart';
+import 'package:expenseo/features/saving/domain/entity/saving_goal.dart';
 import 'package:expenseo/features/saving/presentation/cubit/saving_cubit.dart';
 import 'package:expenseo/features/saving/presentation/cubit/saving_state.dart';
 import 'package:expenseo/features/saving/presentation/widget/deposit_list.dart';
@@ -6,38 +6,36 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class GoalDetailPage extends StatelessWidget {
-  const GoalDetailPage({super.key});
+  final SavingGoal goal;
+
+  const GoalDetailPage({super.key, required this.goal});
 
   @override
   Widget build(BuildContext context) {
     return PopScope(
       onPopInvokedWithResult: (didPop, result) {
-
         if (didPop) {
           context.read<SavingCubit>().getAllGoal();
         }
       },
       child: Scaffold(
-        appBar: AppBar(
-          title: Text('Deposit History' , style: AppTextStyles.h4(),),
-        ),
-        body: BlocBuilder<SavingCubit , SavingState>(
-            builder: (context , state){
-              if (state is SavingLoading){
-                return const Center(child: CircularProgressIndicator());
-              }
-
-              if(state is SavingError){
-                return Center(child: Text(state.message),);
-              }
-
-              if(state is DepositLoaded){
-                return DepositList(deposits: state.deposits);
-              }
-
-              return const SizedBox.shrink();
+        body: BlocBuilder<SavingCubit, SavingState>(
+          builder: (context, state) {
+            if (state is SavingLoading) {
+              return const Center(child: CircularProgressIndicator());
             }
-        )
+
+            if (state is SavingError) {
+              return Center(child: Text(state.message));
+            }
+
+            if (state is DepositLoaded) {
+              return DepositList(deposits: state.deposits, goal: goal);
+            }
+
+            return const SizedBox.shrink();
+          },
+        ),
       ),
     );
   }
