f66810ddccab3dbf05976f6550d2a84a86128cf9
viren <virenrangani7@gmail.com>
2026-05-29 09:46:24 +0530

    feat: add investment entity class

---
+++ lib/features/investment/domain/entity/investment.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class InvestmentModel {
+  final String title;
+  final IconData icon;
+  final Color color;
+  // final Widget page;
+
+  InvestmentModel({
+    required this.title,
+    required this.icon,
+    required this.color,
+    // required this.page,
+  });
+}
