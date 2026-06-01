b27ecb8a622a30e554843885e187d1644cf0a246
viren <virenrangani7@gmail.com>
2026-06-01 09:58:05 +0530

    feat: add class AddStockPage

---
+++ lib/features/investment/main_page/stocks/presentation/add_stock_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class AddStockPage extends StatelessWidget {
+  const AddStockPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: []);
+  }
+}
