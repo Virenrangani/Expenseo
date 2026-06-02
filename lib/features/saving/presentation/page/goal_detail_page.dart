ddcc7f3df190f8308a7db0e22cd8b5a5d191ee74
viren <virenrangani7@gmail.com>
2026-06-02 14:22:39 +0530

    feat: add initState and read getAllDeposit method and pass ShowingGoalData class

--- lib/features/saving/presentation/page/goal_detail_page.dart
+++ lib/features/saving/presentation/page/goal_detail_page.dart
@@ -1,14 +1,24 @@
 import 'package:expenseo/features/saving/domain/entity/saving_goal.dart';
 import 'package:expenseo/features/saving/presentation/cubit/saving_cubit.dart';
-import 'package:expenseo/features/saving/presentation/cubit/saving_state.dart';
-import 'package:expenseo/features/saving/presentation/widget/deposit_list.dart';
+import 'package:expenseo/features/saving/presentation/widget/showing_goal_data.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class GoalDetailPage extends StatelessWidget {
+class GoalDetailPage extends StatefulWidget {
   final SavingGoal goal;
 
   const GoalDetailPage({super.key, required this.goal});
+
+  @override
+  State<GoalDetailPage> createState() => _GoalDetailPageState();
+}
+
+class _GoalDetailPageState extends State<GoalDetailPage> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<SavingCubit>().getAllDeposit(widget.goal.id);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,25 +28,7 @@ class GoalDetailPage extends StatelessWidget {
           context.read<SavingCubit>().getAllGoal();
         }
       },
-      child: Scaffold(
-        body: BlocBuilder<SavingCubit, SavingState>(
-          builder: (context, state) {
-            if (state is SavingLoading) {
-              return const Center(child: CircularProgressIndicator());
-            }
-
-            if (state is SavingError) {
-              return Center(child: Text(state.message));
-            }
-
-            if (state is DepositLoaded) {
-              return DepositList(deposits: state.deposits, goal: goal);
-            }
-
-            return const SizedBox.shrink();
-          },
-        ),
-      ),
+      child: Scaffold(body: ShowingGoalData(goal: widget.goal)),
     );
   }
 }
