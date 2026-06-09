a03da4e34064848af34831eaba45431a933e7e50
Virenrangani <virenrangani7@gmail.com>
2026-06-09 07:31:00 +0530

    feat: add sign up remote data source with api call

---
+++ lib/features/auth/data/data_source/sign_up_remote_data_source.dart
@@ -0,0 +1,39 @@
+import 'package:dio/dio.dart';
+
+import '../../../../core/constant/string/app_string.dart';
+import '../../../../core/storage/shared_pref/shared_pref_service.dart';
+import '../model/user_model.dart';
+
+abstract class SignUpRemoteDataSource {
+  Future<UserModel> signUp(String email, String name, String password);
+}
+
+class SignUpRemoteDataSourceImpl extends SignUpRemoteDataSource {
+  final Dio dio;
+
+  SignUpRemoteDataSourceImpl(this.dio);
+
+  @override
+  Future<UserModel> signUp(String email, String name, String password) async {
+    try {
+      final response = await dio.post<Map<String, dynamic>>(
+        '/signup',
+        data: {'email': email, 'name': name, 'password': password},
+      );
+
+      final user = UserModel.fromJson(response.data!);
+
+      await SharedPrefService.saveUser(
+        id: user.id,
+        email: user.email,
+        name: user.name,
+      );
+
+      return user;
+    } on DioException catch (e) {
+      throw Exception(e.response?.data['message'] ?? 'Registration failed');
+    } catch (_) {
+      throw Exception(AppString.somethingWentWrong);
+    }
+  }
+}
