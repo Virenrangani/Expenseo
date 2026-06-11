02d22b62f72468de803052eacd06eeeabe5efe4d
Virenrangani <virenrangani7@gmail.com>
2026-06-11 21:13:46 +0530

    feat: add token field

--- lib/features/auth/data/model/user_model.dart
+++ lib/features/auth/data/model/user_model.dart
@@ -2,18 +2,20 @@ class UserModel {
   final String id;
   final String email;
   final String name;
+  final String token;
 
-  UserModel({required this.id, required this.email, required this.name});
+  UserModel({required this.id, required this.email, required this.name, required this.token});
 
   factory UserModel.fromJson(Map<String, dynamic> json) {
     return UserModel(
       id: (json['id'] ?? '').toString(),
       email: (json['email'] ?? '').toString(),
       name: (json['name'] ?? '').toString(),
+      token: (json['token'] ?? '').toString(),
     );
   }
 
   Map<String, dynamic> toJson() {
-    return {'id': id, 'email': email, 'name': name};
+    return {'id': id, 'email': email, 'name': name,'token':token,};
   }
 }
