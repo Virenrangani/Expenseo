f41b390506ed62e879ab17256dc5272ae1672a80
Virenrangani <virenrangani7@gmail.com>
2026-06-13 08:32:43 +0530

    feat: add forgot and reset password with the api calling

---
+++ lib/features/forgot_password/data/data_source/forgot_password_data_source.dart
@@ -0,0 +1,46 @@
+import 'package:dio/dio.dart';
+
+abstract class ForgotPasswordDataSource {
+  Future<void> forgotPassword(String email);
+  Future<void> resetPassword(String email, String password,String otp);
+}
+
+class ForgotPasswordDataSourceImpl extends ForgotPasswordDataSource {
+  final Dio dio;
+  ForgotPasswordDataSourceImpl(this.dio);
+  @override
+  Future<void> forgotPassword(String email) async {
+    try{
+      await dio.post<String>('/forgot-password', data: {
+        'email': email,
+      });
+    }on DioException catch (e) {
+      final data = e.response?.data;
+
+      final message =
+      data is Map<String, dynamic>
+          ? data['message']?.toString()
+          : 'Login failed';
+
+      throw Exception(message);
+    }
+  }
+
+  @override
+  Future<void> resetPassword(String email , String password , String otp) async {
+    try{
+      await dio.post<String>('/reset-password', data: {
+        'email':email,
+        'password': password,
+        'otp':otp
+      });
+    }on DioException catch (e) {
+      final data = e.response?.data;
+      final message =
+      data is Map<String, dynamic>
+          ? data['message']?.toString()
+          : 'Login failed';
+      throw Exception(message);
+    }
+  }
+}
