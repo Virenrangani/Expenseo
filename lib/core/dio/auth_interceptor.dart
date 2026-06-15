ea946c5152f839c4bc08ac8ff76d91146ce40bf9
viren <virenrangani7@gmail.com>
2026-06-15 16:31:06 +0530

    refactor: change the OnError with proper refresh token creation

--- lib/core/dio/auth_interceptor.dart
+++ lib/core/dio/auth_interceptor.dart
@@ -1,23 +1,64 @@
 import 'package:dio/dio.dart';
-
-import 'token_storage.dart';
+import 'package:expenseo/core/dio/refresh_token_service.dart';
+import 'package:expenseo/core/dio/token_storage.dart';
 
 class AuthInterceptor extends Interceptor {
+  final Dio dio;
   final TokenStorage tokenStorage;
+  final RefreshTokenService refreshService;
 
-  AuthInterceptor(this.tokenStorage);
+  bool _isRefreshing = false;
+
+  Future<String?>? _refreshFuture;
+
+  AuthInterceptor({
+    required this.dio,
+    required this.tokenStorage,
+    required this.refreshService,
+  });
 
   @override
-  Future<void> onRequest(
-    RequestOptions options,
-    RequestInterceptorHandler handler,
+  Future<void> onError(
+    DioException err,
+    ErrorInterceptorHandler handler,
   ) async {
-    final token = await tokenStorage.getAccessToken();
-
-    if (token != null && token.isNotEmpty) {
-      options.headers['Authorization'] = 'Bearer $token';
+    if (err.response?.statusCode != 401) {
+      return handler.next(err);
     }
 
-    handler.next(options);
+    final request = err.requestOptions;
+
+    if (request.path.contains('/auth/refresh')) {
+      return handler.next(err);
+    }
+
+    try {
+      if (!_isRefreshing) {
+        _isRefreshing = true;
+
+        _refreshFuture = refreshService.refresh();
+
+        final token = await _refreshFuture;
+
+        _isRefreshing = false;
+
+        if (token == null) {
+          await tokenStorage.clear();
+          return handler.next(err);
+        }
+      }
+
+      final newToken = await _refreshFuture;
+
+      request.headers['Authorization'] = 'Bearer $newToken';
+
+      final response = await dio.fetch(request);
+
+      return handler.resolve(response);
+    } catch (_) {
+      await tokenStorage.clear();
+
+      return handler.next(err);
+    }
   }
 }
