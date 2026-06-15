7952468ad37b78a490eb095b5a0eb23df1d1e31a
viren <virenrangani7@gmail.com>
2026-06-15 16:30:00 +0530

    feat: add RefreshTokenResponse and create refreshToken api calling with this class

---
+++ lib/core/dio/auth_api.dart
@@ -0,0 +1,34 @@
+import 'package:dio/dio.dart';
+
+class RefreshTokenResponse {
+  final String accessToken;
+  final String refreshToken;
+
+  const RefreshTokenResponse({
+    required this.accessToken,
+    required this.refreshToken,
+  });
+
+  factory RefreshTokenResponse.fromJson(Map<String, dynamic> json) {
+    return RefreshTokenResponse(
+      accessToken: json['accessToken'] as String,
+      refreshToken: json['refreshToken'] as String,
+    );
+  }
+}
+
+class AuthApi {
+  final Dio dio;
+
+  AuthApi(this.dio);
+
+  Future<RefreshTokenResponse> refreshToken(String refreshToken) async {
+    final response = await dio.post<Map<String, dynamic>>(
+      '/auth/refresh',
+      data: {'refreshToken': refreshToken},
+      options: Options(headers: {'Authorization': null}),
+    );
+
+    return RefreshTokenResponse.fromJson(response.data!);
+  }
+}
