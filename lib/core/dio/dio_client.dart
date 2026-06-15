a04caca0e4b94a27c70785397ed9315291e7c58c
viren <virenrangani7@gmail.com>
2026-06-15 16:33:01 +0530

    feat: add refreshService and authApi in Dio client

--- lib/core/dio/dio_client.dart
+++ lib/core/dio/dio_client.dart
@@ -1,32 +1,45 @@
 import 'package:dio/dio.dart';
-import 'package:expenseo/core/constant/api_url/base_url.dart';
 import 'package:flutter/foundation.dart';
 
+import '../constant/api_url/base_url.dart';
+import 'auth_api.dart';
 import 'auth_interceptor.dart';
+import 'refresh_token_service.dart';
 import 'token_storage.dart';
 
 class DioClient {
-  static Dio create(TokenStorage tokenStorage) {
-    final dio = Dio(
-      BaseOptions(
-        baseUrl: BaseUrl.baseUrl,
-        connectTimeout: const Duration(seconds: 30),
-        receiveTimeout: const Duration(seconds: 30),
-        sendTimeout: const Duration(seconds: 30),
-        contentType: Headers.jsonContentType,
-        headers: {'Accept': 'application/json'},
+  static Dio create({required TokenStorage tokenStorage}) {
+    final baseOptions = BaseOptions(
+      baseUrl: BaseUrl.baseUrl,
+      connectTimeout: const Duration(seconds: 30),
+      receiveTimeout: const Duration(seconds: 30),
+      sendTimeout: const Duration(seconds: 30),
+      contentType: Headers.jsonContentType,
+      headers: {'Accept': 'application/json'},
+    );
+
+    final dio = Dio(baseOptions);
+
+    final authDio = Dio(baseOptions);
+
+    final authApi = AuthApi(authDio);
+
+    final refreshService = RefreshTokenService(
+      authApi: authApi,
+      tokenStorage: tokenStorage,
+    );
+
+    dio.interceptors.add(
+      AuthInterceptor(
+        dio: dio,
+        tokenStorage: tokenStorage,
+        refreshService: refreshService,
       ),
     );
 
-    dio.interceptors.add(AuthInterceptor(tokenStorage));
-
     if (kDebugMode) {
       dio.interceptors.add(
-        LogInterceptor(
-          requestBody: true,
-          responseBody: true,
-          responseHeader: false,
-        ),
+        LogInterceptor(requestBody: true, responseBody: true),
       );
     }
 
