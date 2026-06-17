3bd0bd1a80fba8807dc522cc4f01eeab6b4a35df
Virenrangani <virenrangani7@gmail.com>
2026-06-17 07:22:16 +0530

    refactor: change the parameter to remote data source

--- lib/features/auth/data/repository_impl/login_repository_impl.dart
+++ lib/features/auth/data/repository_impl/login_repository_impl.dart
@@ -1,10 +1,12 @@
-import 'package:expenseo/features/auth/data/data_source/login_data_source.dart';
+import 'package:expenseo/features/auth/data/data_source/login_remote_data_source.dart';
 import 'package:expenseo/features/auth/domain/entity/user.dart';
 import 'package:expenseo/features/auth/domain/repository/log_in_repository.dart';
 
 class LoginRepositoryImpl extends LogInRepository{
-  final LoginDataSource dataSource;
+  final LoginRemoteDataSource dataSource;
+
   LoginRepositoryImpl(this.dataSource);
+
   @override
   Future<User> login(String email, String password) async{
     final result=await dataSource.login(email, password);
@@ -15,8 +17,8 @@ class LoginRepositoryImpl extends LogInRepository{
   }
 
   @override
-  Future<User> signInWithGoogle() async {
-    final model = await dataSource.signInWithGoogle();
-    return User(id: model.id, email: model.email, name: model.name);
+  Future<User> signInWithGoogle() {
+    // TODO: implement signInWithGoogle
+    throw UnimplementedError();
   }
 }
