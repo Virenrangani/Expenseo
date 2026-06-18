68faef40671253830c86d2e3b64f838a2d227bcb
Virenrangani <virenrangani7@gmail.com>
2026-06-18 07:28:27 +0530

    feat: add expense remote data source with post , get and delete api call

--- lib/features/expense/data/data_source/expense_remote_data_source.dart
+++ lib/features/expense/data/data_source/expense_remote_data_source.dart
@@ -3,7 +3,7 @@ import 'package:dio/dio.dart' show Dio, DioException;
 import '../model/expense_model.dart';
 
 abstract class ExpenseRemoteDataSource {
-  Future<void> addExpense(String uid, ExpenseModel expense);
+  Future<void> addExpense(ExpenseModel expense);
   Future<List<ExpenseModel>> getExpense(String uid);
   Future<void> removeExpense(String uid, String expenseId);
 }
@@ -14,7 +14,7 @@ class ExpenseRemoteDataSourceImpl extends ExpenseRemoteDataSource {
   final Dio dio;
 
   @override
-  Future<void> addExpense(String uid, ExpenseModel expense) async {
+  Future<void> addExpense(ExpenseModel expense) async {
     try {
       await dio.post<Map<String, dynamic>>('/expenses', data: expense.toJson());
     } on DioException catch (e) {
@@ -22,41 +22,45 @@ class ExpenseRemoteDataSourceImpl extends ExpenseRemoteDataSource {
 
       final message = data is Map<String, dynamic>
           ? data['message']?.toString()
-          : 'Expense is not added';
+          : 'Failed to create expense';
 
       throw Exception(message);
     }
   }
 
-  // @override
-  // Future<List<ExpenseModel>> getExpense(String uid) async {
-  //   try {
-  //     final response = await dio.get<List<Map<String, dynamic>>>('/expenses');
-  //
-  //     final data = response.data ?? [];
-  //
-  //     return data;
-  //     // return data.map(ExpenseModel.fromJson).toList();
-  //   } on DioException catch (e) {
-  //     final data = e.response?.data;
-  //
-  //     final message = data is Map<String, dynamic>
-  //         ? data['message']?.toString()
-  //         : 'Failed to fetch expenses';
-  //
-  //     throw Exception(message);
-  //   }
-  // }
-
   @override
-  Future<void> removeExpense(String uid, String expenseId) {
-    // TODO: implement removeExpense
-    throw UnimplementedError();
+  Future<List<ExpenseModel>> getExpense(String uid) async {
+    try {
+      final response = await dio.get<List<dynamic>>('/expenses');
+
+      final data = response.data!;
+
+      return data
+          .map((json) => ExpenseModel.fromJson(json as Map<String, dynamic>))
+          .toList();
+    } on DioException catch (e) {
+      final data = e.response?.data;
+
+      final message = data is Map<String, dynamic>
+          ? data['message']?.toString()
+          : 'Failed to fetch expenses';
+
+      throw Exception(message);
+    }
   }
 
   @override
-  Future<List<ExpenseModel>> getExpense(String uid) {
-    // TODO: implement getExpense
-    throw UnimplementedError();
+  Future<void> removeExpense(String uid, String expenseId) async {
+    try {
+      await dio.delete<String>('/expenses/$uid');
+    } on DioException catch (e) {
+      final data = e.response?.data;
+
+      final message = data is Map<String, dynamic>
+          ? data['message']?.toString()
+          : 'Failed to delete expense';
+
+      throw Exception(message);
+    }
   }
 }
