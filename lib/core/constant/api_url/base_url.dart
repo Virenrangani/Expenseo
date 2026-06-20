ae20e250cf441436666c1ed94a500512ef13c58f
Virenrangani <virenrangani7@gmail.com>
2026-06-20 16:44:08 +0530

    refactor: update base url

--- lib/core/constant/api_url/base_url.dart
+++ lib/core/constant/api_url/base_url.dart
@@ -1,4 +1,5 @@
 class BaseUrl {
-  static const String baseUrl = 'http://localhost:8080/api/auth';
-  // static const String baseUrl = 'http://192.168.1.15:8080/api/auth';
+  // static const String baseUrl = 'http://localhost:8080/api';
+  // static const String baseUrl = 'http://192.168.1.15:8080/api';
+  static const String baseUrl = 'http://10.69.99.4:8080/api';
 }
