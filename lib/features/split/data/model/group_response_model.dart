9d8639d03a7749c40c899821706971a0fe4e299d
Viren <viren.rangani@tatvasoft.com>
2026-06-23 14:34:34 +0530

    feat: add members parameter and update the fromJson

--- lib/features/split/data/model/group_response_model.dart
+++ lib/features/split/data/model/group_response_model.dart
@@ -1,20 +1,28 @@
+import 'package:expenseo/features/split/data/model/user_model.dart';
+
 class GroupResponseModel {
   final String id;
   final String name;
   final DateTime createdAt;
+  final List<UserModel> members;
 
   const GroupResponseModel({
     required this.id,
     required this.name,
     required this.createdAt,
+    required this.members,
   });
 
-  factory GroupResponseModel.fromJson(
-      Map<String, dynamic> json) {
+  factory GroupResponseModel.fromJson(Map<String, dynamic> json) {
     return GroupResponseModel(
       id: json['id'].toString(),
       name: json['name'].toString(),
       createdAt: DateTime.parse(json['createdAt'].toString()),
+      members:
+          (json['members'] as List<dynamic>?)
+              ?.map((e) => UserModel.fromJson(e as Map<String, dynamic>))
+              .toList() ??
+          [],
     );
   }
 }
