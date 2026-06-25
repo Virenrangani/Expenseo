74b8cc10b825538576f79511b67e972d5aac70c3
Virenrangani <virenrangani7@gmail.com>
2026-06-25 07:32:07 +0530

    refactor: change parameter remote data source and implementing the method using it

--- lib/features/saving/data/repository_impl/saving_repository_impl.dart
+++ lib/features/saving/data/repository_impl/saving_repository_impl.dart
@@ -1,4 +1,4 @@
-import 'package:expenseo/features/saving/data/data_source/saving_datasource.dart';
+import 'package:expenseo/features/saving/data/data_source/saving_remote_data_source.dart';
 import 'package:expenseo/features/saving/data/model/deposit_model.dart';
 import 'package:expenseo/features/saving/data/model/saving_model.dart';
 import 'package:expenseo/features/saving/domain/entity/deposit.dart';
@@ -6,30 +6,30 @@ import 'package:expenseo/features/saving/domain/entity/saving_goal.dart';
 import 'package:expenseo/features/saving/domain/repository/saving_repository.dart';
 
 class SavingRepositoryImpl extends SavingRepository{
-  final SavingDatasource savingDatasource;
+  final SavingRemoteDataSource datasource;
   
-  SavingRepositoryImpl(this.savingDatasource);
+  SavingRepositoryImpl(this.datasource);
 
 
   @override
   Future<void> createGoal(SavingGoal savingGoal) async {
-    await savingDatasource.createGoal(SavingModel.fromEntity(savingGoal));
+    await datasource.createGoal(SavingModel.fromEntity(savingGoal));
   }
 
   @override
   Future<List<SavingGoal>> getAllGoal() async{
-    final models = await savingDatasource.getAllGoal();
+    final models = await datasource.getAllGoal();
     return models.map((m) => m.toEntity()).toList();
   }
 
   @override
   Future<void> addSavingAmount(Deposit deposit) async {
-    await savingDatasource.addSavingAmount(DepositModel.fromEntity(deposit));
+    await datasource.addSavingAmount(DepositModel.fromEntity(deposit));
   }
 
   @override
   Future<List<Deposit>> getAllDeposit(String goalId) async{
-     final deposits = await savingDatasource.getAllDeposit(goalId);
+     final deposits = await datasource.getAllDeposit(goalId);
      return deposits.map( (d) => d.toEntity()).toList();
   }
 }
